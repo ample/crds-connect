246c670e40ff434bdf349e56e69a810bc6d5df84
Matt Silbernagel <silbermm@gmail.com>
2017-01-30 15:15:49 -0500

    it's a start

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,56 +2,60 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
-var Dotenv = require('dotenv-webpack');
+var Dotenv = require('webpack-dotenv-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const PHOENIX_SERVER = process.env.MIX_ENV || false;
 
 module.exports = {
   entry: {
-    'polyfills': ['./src/polyfills.ts'],
-    'vendor': ['./src/vendor.ts'],
-    'app': ['./src/main.ts']
+    'polyfills': [helpers.root('./src/polyfills.ts')],
+    'vendor': [helpers.root('./src/vendor.ts')],
+    'app': [helpers.root('./src/main.ts')]
   },
 
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['.ts', '.js']
   },
 
   module: {
-    loaders: [
-      {
+    rules: [{
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-      },
-      {
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              configFileName: helpers.root('./tsconfig.json')
+            }
+          },{
+            loader: 'angular2-template-loader'
+          }
+        ],
+        exclude:[/\.(spec|e2e)\.ts$/, 'node_modules']
+      }, {
         test: /\.html$/,
-        loader: 'html'
-      },
-      {
+        loader: 'html-loader'
+      }, {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      }, {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file"
-      },
-      {
+        loader: "file-loader"
+      }, {
         test: /\.(png|jpe?g|gif|svg|ico)$/,
-        loader: 'file?name=assets/[name].[hash].[ext]'
-      },
-      {
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      }, {
         test: /\.css$/,
         exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-      },
-      {
+        use: ExtractTextPlugin.extract({fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap'})
+      }, {
         test: /\.css$/,
         include: helpers.root('src', 'app'),
-        loader: 'raw'
-      },
-      {
+        use: 'raw-loader'
+      }, {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loaders: ['raw-loader', 'sass-loader']
+        use: ['raw-loader', 'sass-loader']
       }
     ]
   },
@@ -60,23 +64,26 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({
       name: ['app', 'vendor', 'polyfills']
     }),
-    
-    new Dotenv({
-      systemvars: true
-    }),
+
+    //new Dotenv({
+      //systemvars: true,
+      //sample: helpers.root('./.env'),
+      //path: helpers.root('./.env')
+
+    /*}),*/
 
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
 
     new CopyWebpackPlugin([{
-      from: 'src/assets',
-      to: 'assets',
+      from: helpers.root('src/assets'),
+      to: PHOENIX_SERVER ? '/js/crds-connect/assets' : 'assets'
     }], { ignore: ['*.scss', 'mock-data/*'] }),
 
     new CopyWebpackPlugin([
       {
-        from: './apache_site.conf',
+        from: helpers.root('./apache_site.conf'),
         to: 'apache_site.conf',
         transform: function (content, path) {
           return content.toString().replace(/\${(.*?)}/g, function(match, p1, offset, string) {          
@@ -84,10 +91,10 @@ module.exports = {
           });
         }
       },
-      { 
+      {
         context: 'node_modules/bootstrap-sass/assets/fonts/bootstrap',
         from: '**/*', 
-        to: 'fonts/' 
+        to: PHOENIX_SERVER ? '/js/crds-connect/fonts/' : 'fonts/'
       },
     ])
   ]
