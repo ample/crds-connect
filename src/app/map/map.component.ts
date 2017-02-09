05f21762825ce69c530c0cee86f607bd240d8a64
vchtelmakh <vchtelmakh@callibrity.com>
2017-02-09 15:23:32 -0500

    Simplify code

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-
-import { APIService } from '../services/api.service';
-import { LoginRedirectService } from '../services/login-redirect.service';
-import { StateService } from '../services/state.service';
-import { StoreService } from '../services/store.service';
 
 @Component({
   selector: 'app-map',
@@ -22,12 +15,6 @@ export class MapComponent implements OnInit {
 
 
   constructor(
-    private api: APIService,
-    private fb: FormBuilder,
-    private router: Router,
-    private redirectService: LoginRedirectService,
-    private state: StateService,
-    private store: StoreService
   ) { }
 
   public ngOnInit(): void {
