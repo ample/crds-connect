e26ccc2bad2cfc249915cdb7f68013ad66e8683c
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-02-15 13:18:40 -0500

    1 method tested.

--- src/app/pin-details/pin-details.component.spec.ts
+++ src/app/pin-details/pin-details.component.spec.ts
@@ -1,9 +1,9 @@
 /* tslint:disable:no-unused-variable */
 import { Angulartics2 } from 'angulartics2';
 import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpModule, JsonpModule  } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { APIService } from '../services/api.service';
@@ -48,12 +48,46 @@ describe('Component: Pin-Details component', () => {
     });
     this.fixture = TestBed.createComponent(PinDetailsComponent);
     this.component = this.fixture.componentInstance;
+    this.component.pin = {
+      'firstName': 'Joe',
+      'lastName': 'Kerstanoff',
+      'emailAddress': 'jkerstanoff@callibrity.com',
+      'contactId': 2562378,
+      'participantId': 7537153,
+      'address': {
+        'addressId': 5272699,
+        'addressLine1': '8854 Penfield Way',
+        'addressLine2': null,
+        'city': 'Maineville',
+        'state': 'OH',
+        'zip': '45039-9731',
+        'foreignCountry': 'United States',
+        'county': null,
+        'longitude': null,
+        'latitude': null
+      },
+      'hostStatus': 0,
+      'gathering': null
+    };
 
   });
 
   it('should create an instance', () => {
     expect(this.component).toBeTruthy();
   });
+
+  it('doesLoggedInUserOwnPin() should return true if contactId matches', inject([SessionService], (service:any) => {
+    spyOn(service, 'getContactId').and.returnValue(2562378);
+    let returnValue = this.component.doesLoggedInUserOwnPin();
+    expect(returnValue).toBe(true);
+
+  }));
+
+  it('doesLoggedInUserOwnPin() should return false if contactId doesnt match', inject([SessionService], (service:any) => {
+    spyOn(service, 'getContactId').and.returnValue(42);
+    let returnValue = this.component.doesLoggedInUserOwnPin();
+    expect(returnValue).toBe(false);
+  }));
 
 });
 
