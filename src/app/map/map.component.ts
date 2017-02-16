7db1965300e244258acdda7d0ba89c79a66b7587
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-02-16 11:25:52 -0500

    add unit tests for user-location.service

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Router } from '@angular/router';
 
-import { APIService } from '../services/api.service';
 import { crdsOakleyCoords } from '../shared/constants';
-import { LoginRedirectService } from '../services/login-redirect.service';
-import { LocationService } from '../services/location.service';
 import { MapSettings } from '../models/map-settings';
 import { UserLocationService } from  '../services/user-location.service';
 
@@ -19,11 +16,7 @@ export class MapComponent implements OnInit {
   public mapSettings: MapSettings  = new MapSettings(crdsOakleyCoords.lat, crdsOakleyCoords.lng, 15, false, true);
   public address: string;
 
-  constructor(private api: APIService,
-              private loginRedirectService: LoginRedirectService,
-              private router: Router,
-              private locationService: LocationService,
-              private userLocationService: UserLocationService) {
+  constructor( private userLocationService: UserLocationService) {
   }
 
   public ngOnInit(): void {
--- src/app/services/user-location.spec.ts
+++ src/app/services/user-location.spec.ts
@@ -5,10 +5,45 @@ import { GeoCoordinates } from '../models/geo-coordinates';
 import { SessionService } from './session.service';
 import { UserLocationService } from './user-location.service';
 
+import { ContentService } from '../services/content.service';
+import { IFrameParentService } from '../services/iframe-parent.service';
+import { APIService } from '../services/api.service';
+import { StateService } from '../services/state.service';
+import { StoreService } from '../services/store.service';
+import { LoginRedirectService } from '../services/login-redirect.service';
+import { Angulartics2 } from 'angulartics2';
+import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule, JsonpModule  } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { LocationService } from '../services/location.service';
+import { PinService}  from '../services/pin.service';
+
 describe('Service: User-Location', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-        providers: [UserLocationService]
+        imports: [
+          RouterTestingModule.withRoutes([]),
+          HttpModule,
+          JsonpModule,
+          ReactiveFormsModule,
+          AlertModule
+        ],
+        providers: [
+          UserLocationService,
+          LocationService,
+          PinService,
+          IFrameParentService,
+          StoreService,
+          StateService,
+          APIService,
+          SessionService,
+          CookieService,
+          Angulartics2,
+          ContentService,
+          LoginRedirectService
+        ]
     });
   });
 
@@ -16,18 +51,18 @@ describe('Service: User-Location', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('Should get the user\'s current position as a GeoCoordinates object', async(inject(
+  it('Should get the default Crds-Oakley coordinates', inject([UserLocationService], (service: any) => {
+    expect(service.getUserLocationFromDefault()).toEqual(new GeoCoordinates(crdsOakleyCoords.lat, crdsOakleyCoords.long));
+  }));
+
+  it('Should get a location as a GeoCoordinates object', async(inject(
     [UserLocationService],
     (service) => {
-      const resultObs = service.getCurrentPosition();
+      const resultObs = service.GetUserLocation();
 
       resultObs.subscribe(res => {
         expect(res).toEqual(jasmine.any(GeoCoordinates));
       });
   })));
-
-  it('Should get the default Crds-Oakley coordinates', inject([UserLocationService], (service: any) => {
-    expect(service.getDefaultPosition()).toEqual(new GeoCoordinates(crdsOakleyCoords.lat, crdsOakleyCoords.long));
-  }));
 
 });
