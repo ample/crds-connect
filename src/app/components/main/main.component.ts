864096f81a5f86496bfc96c31d3749e04e09fd0d
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-02-27 07:45:02 -0500

    new component folder

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: 'main.component.html'
+})
+export class MainComponent {
+
+  constructor( ) {}
+
+}
