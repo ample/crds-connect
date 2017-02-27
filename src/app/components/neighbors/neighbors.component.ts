3bddf67228ea12a496058be6e5f12192099657b6
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-02-27 16:41:46 -0500

    US6286 WIP

--- src/app/components/neighbors/neighbors.component.ts
+++ src/app/components/neighbors/neighbors.component.ts
@@ -4,7 +4,11 @@ import { Router } from '@angular/router';
 
 import { APIService } from '../../services/api.service';
 
+import { Pin } from '../../models/pin';
 import { PinSearchResultsDto } from '../../models/pin-search-results-dto';
+import { GeoCoordinates } from '../../models/geo-coordinates';
+
+import { crdsOakleyCoords } from '../../shared/constants';
 
 @Component({
   selector: 'app-neighbors',
@@ -24,20 +28,13 @@ export class NeighborsComponent {
   doSearch(searchString: string) {
     console.log(searchString);
 
-    // this.api.getPinsAddressSearchResults(searchString).subscribe(
-    //   pins => {
-    //   this.pinSearchResults = pins;
-    //   },
-    //   err => {
-    //     console.log('error getting search results');
-    //   }
-    // );
-
     let mockApiCall = new Observable(observer => {
       let rand = Math.random();
+      let pins: Pin[];
+      this.pinSearchResults = new PinSearchResultsDto(new GeoCoordinates(26.1844000, 91.7458000), pins);
       setTimeout(() => {
         if (rand > .5) {
-          observer.next('Success');
+          observer.next(this.pinSearchResults);
         } else {
           observer.error('Error!');
         }
@@ -45,8 +42,23 @@ export class NeighborsComponent {
     });
 
     mockApiCall.subscribe(
-      next => console.log(next),
+      next => {
+        console.log(next);
+        // output this to the app-map child component
+          // public mapSettings: MapSettings  = new MapSettings(crdsOakleyCoords.lat, crdsOakleyCoords.lng, 15, false, true);
+        // this.pinSearchResults.centerLocation.lat;
+        // this.pinSearchResults.centerLocation.lng;
+      },
       err => console.log(err)
       );
+
+    // this.api.getPinsAddressSearchResults(searchString).subscribe(
+    //   pins => {
+    //   this.pinSearchResults = pins;
+    //   },
+    //   err => {
+    //     console.log('error getting search results');
+    //   }
+    // );      
   }
 }
