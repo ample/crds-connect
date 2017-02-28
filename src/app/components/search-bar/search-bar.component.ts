1629ee008f40781b2439065ad2c8cee39657a8c6
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-02-27 22:09:38 -0500

    US6286 getting initial coordinates, then change on submit of location bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, Output, EventEmitter } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Router } from '@angular/router';
+
+import { UserLocationService } from  '../../services/user-location.service';
 
 import { GeoCoordinates } from '../../models/geo-coordinates';
 import { Pin } from '../../models/pin';
@@ -11,7 +13,7 @@ import { PinSearchResultsDto } from '../../models/pin-search-results-dto';
   templateUrl: 'search-bar.component.html',
   styleUrls: ['search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
   @Input() buttontext;
   @Output() viewMap: EventEmitter<boolean>  = new EventEmitter<boolean>();
   // @Output() search: EventEmitter<string> = new EventEmitter<string>();
@@ -20,7 +22,18 @@ export class SearchBarComponent {
   private searchText: string = '';
   public pinSearchResults: PinSearchResultsDto;
 
-  constructor( ) {}
+  constructor(private userLocationService: UserLocationService) {}
+
+  public ngOnInit(): void {
+    let haveResults = !!this.pinSearchResults;
+    if (!haveResults) {
+      this.userLocationService.GetUserLocation().subscribe(
+        pos => {
+          this.pinSearchResults = new PinSearchResultsDto(new GeoCoordinates(pos.lat, pos.lng), new Array<Pin>());
+        }
+      );
+    }
+  }
 
   public toggleView() {
     this.buttontext = 'toggle';
