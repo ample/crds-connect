b30607f001860e9931aceda95101724d23b88527
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-03-10 07:48:13 -0500

    Fix tests

--- src/app/components/pin-details/participants-list/participant-card/participant-card.component.spec.ts
+++ src/app/components/pin-details/participants-list/participant-card/participant-card.component.spec.ts
@@ -2,12 +2,22 @@
 import { Angulartics2 } from 'angulartics2';
 import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SessionService } from '../../../../services/session.service';
+import { HttpModule, JsonpModule } from '@angular/http';
+
+import { AddressFormComponent } from '../../../address-form/address-form.component';
 import { GatheringComponent } from '../../gathering/gathering.component';
 import { ParticipantCardComponent } from './participant-card.component';
 import { ParticipantsListComponent } from '../participants-list.component';
 import { Participant } from '../../../../models/participant';
+import { PinDetailsComponent } from '../../pin-details.component';
+import { PinHeaderComponent } from '../../pin-header/pin-header.component';
+import { SayHiComponent } from '../../say-hi/say-hi.component';
+import { ReadonlyAddressComponent } from '../../readonly-address/readonly-address.component';
+import { PinLoginActionsComponent } from '../../pin-login-actions/pin-login-actions.component';
+import { PersonComponent } from '../../person/person.component';
 
 
 describe('Component: Participant Card component', () => {
@@ -19,16 +29,21 @@ describe('Component: Participant Card component', () => {
 
   describe('Participant Card', () => {
     beforeEach(() => {
-      participant = new Participant();
-      participant.participantId = 123;
-      participant.contactId = 321;     
+      participant = new Participant('Mason', 321, 'Kerstanoff, Joeker', 'email@email.com', 111, 22, 'Leader', true, 'Kerstanoff', 'JoeKer', 123, '1943-02-03');
       TestBed.configureTestingModule({
         declarations: [
+          AddressFormComponent,
           GatheringComponent,
           ParticipantCardComponent,
-          ParticipantsListComponent
+          ParticipantsListComponent,
+          PersonComponent,
+          PinDetailsComponent,
+          PinHeaderComponent,
+          PinLoginActionsComponent,
+          ReadonlyAddressComponent,
+          SayHiComponent
         ],
-        imports: [],
+        imports: [ ReactiveFormsModule, HttpModule, JsonpModule ],
         providers: [
           SessionService,
           CookieService,
@@ -38,17 +53,12 @@ describe('Component: Participant Card component', () => {
       this.fixture = TestBed.createComponent(ParticipantCardComponent);
       this.component = this.fixture.componentInstance;
       this.component.participant = participant;
-      
-
     });
 
     it('should create an instance', () => {
       expect(this.component).toBeTruthy();
     });
-
-
-
-   
+});
 });
 
 
