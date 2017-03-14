3b580ac0a3d001d86f4277f449f28a3c31f07cec
David Anderson <davidanderson@Davids-MacBook-Pro.local>
2017-03-14 16:10:11 -0400

    just let doSearch do it’s work

--- src/app/components/neighbors/neighbors.component.ts
+++ src/app/components/neighbors/neighbors.component.ts
@@ -47,7 +47,6 @@ export class NeighborsComponent implements OnInit {
     this.state.setLoading(true);
     this.api.getPinsAddressSearchResults(searchString, lat, lng).subscribe(
       next => {
-        console.log(next);
         this.pinSearchResults = next as PinSearchResultsDto;
         this.pinSearchResults.pinSearchResults =
           this.pinSearchResults.pinSearchResults.sort(
