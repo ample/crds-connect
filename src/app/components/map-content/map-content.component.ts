4214d18b07a525554f305d69f955bc9cbb2aebf3
vchtelmakh <vchtelmakh@callibrity.com>
2017-03-16 14:56:34 -0400

    Get map bounding rectangles in both geo and screen location, get marker names and geo-locations.

--- src/app/components/map-content/map-content.component.ts
+++ src/app/components/map-content/map-content.component.ts
@@ -28,6 +28,55 @@ export class MapContentComponent implements OnInit {
     this.mapApiWrapper.getNativeMap()
       .then((map)=> {
 
+          var geoBounds = undefined;
+          var elmBounds = undefined;
+
+          var ngGoogleMap = document.getElementsByClassName("sebm-google-map-container")[0];
+          //console.log(ngGoogleMap);
+          //console.log('BOUNDING RECTANGLE: ');
+          console.log(ngGoogleMap.getBoundingClientRect());
+          //console.log('MAP CENTER: ');
+          var mapCenter = map.getCenter();
+          //console.log(mapCenter);
+          // console.log(mapCenter.lat().valueOf());
+          //console.log(mapCenter.lng().valueOf());
+          //console.log('BOUNDS: ');
+          //console.log(map.getBounds());
+
+          // if( map.getBounds() ) {
+          //
+          //   console.log( map.getBounds().getNorthEast().lat().valueOf() );
+          //   console.log( map.getBounds().getNorthEast().lng().valueOf() );
+          //   console.log( map.getBounds().getSouthWest().lat().valueOf() );
+          //   console.log( map.getBounds().getSouthWest().lng().valueOf() );
+          //
+          //   geoBounds = {
+          //     topLeft: { lat: map.getBounds().getSouthWest().lat().valueOf(), lng: map.getBounds().getNorthEast().lng().valueOf() },
+          //     topRight: { lat: map.getBounds().getNorthEast().lat().valueOf(), lng: map.getBounds().getNorthEast().lng().valueOf()},
+          //     bottomLeft: {lat: map.getBounds().getSouthWest().lat().valueOf(), lng: map.getBounds().getSouthWest().lng().valueOf() },
+          //     bottomRight: {lat: map.getBounds().getNorthEast().lat().valueOf(), lng: map.getBounds().getSouthWest().lng().valueOf() },
+          //   };
+          //
+          //   console.log('GEO BOUNDS: ');
+          //   console.log(geoBounds);
+          //
+          // }
+
+          // var googleMapContent = document.getElementsByClassName("sebm-google-map-content")[0];
+          // console.log(googleMapContent);
+          //
+          // var markerList = googleMapContent.getElementsByTagName("sebm-google-map-marker");
+          // console.log(markerList[0] ? markerList[0] : []);
+          //
+          // if( markerList.length > 0 ) {
+          //     for (var i = 0; i < markerList.length; i++ ){
+          //        var marker = markerList[i];
+          //        var markerLabel = marker.getAttribute("ng-reflect-label");
+          //        var markerLat = marker.getAttribute("ng-marker-latitude");
+          //        var markerLng  = marker.getAttribute("ng-marker-longitude");
+          //     }
+          // }
+
         let zoomControlOptions: any = {
           style: google.maps.ControlPosition.small,
           position: google.maps.ControlPosition.RIGHT_CENTER
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -71,4 +71,8 @@ export class MapComponent implements OnInit {
     }
   }
 
+  public getLabelName(pin: Pin) {
+    return (pin.firstName);
+  }
+
 }
