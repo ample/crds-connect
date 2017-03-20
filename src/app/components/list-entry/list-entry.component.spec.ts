5ffe7ec644c6c5a73de223416624f9e4242f1fa9
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-03-20 09:38:51 -0400

    a few more unit tests

---
+++ src/app/components/list-entry/list-entry.component.spec.ts
@@ -0,0 +1,64 @@
+/*
+ * Testing a simple Angular 2 component
+ * More info: https://angular.io/docs/ts/latest/guide/testing.html#!#simple-component-test
+ */
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ListEntryComponent } from './list-entry.component';
+import { StateService } from '../../services/state.service';
+
+describe('ListEntryComponent', () => {
+    let fixture: ComponentFixture<ListEntryComponent>;
+    let comp: ListEntryComponent;
+    let router: Router;
+    let el;
+
+    beforeEach(() => {
+        class RouterStub {
+            navigate(url: string) { return url; }
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [
+                ListEntryComponent
+            ],
+            providers: [
+                StateService,
+                { provide: Router, useClass: RouterStub }
+            ],
+            schemas: [ NO_ERRORS_SCHEMA ]
+        });
+    });
+
+    beforeEach(async(() => {
+        TestBed.compileComponents().then(() => {
+            fixture = TestBed.createComponent(ListEntryComponent);
+            comp = fixture.componentInstance;
+
+            // el = fixture.debugElement.query(By.css('h1'));
+        });
+    }));
+
+    fit('should create an instance', () => {
+        expect(comp).toBeTruthy();
+    });
+
+    fit('should return proper name format', () => {
+        fixture.detectChanges();
+        comp.firstName = 'Bob';
+        comp.lastName = 'Johnson'
+        expect(comp.name()).toBe('BOB J.');
+    });
+
+    fit('should return proper count string', () => {
+        fixture.detectChanges();
+        comp.participantCount = 10;
+        expect(comp.count()).toBe('10 OTHERS');
+    });
+});
