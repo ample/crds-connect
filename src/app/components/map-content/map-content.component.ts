434623d9f625821d93d20c086628bd88dc8ae12d
vchtelmakh <vchtelmakh@callibrity.com>
2017-03-21 10:58:10 -0400

    Syntax

--- src/app/components/map-content/map-content.component.ts
+++ src/app/components/map-content/map-content.component.ts
@@ -64,7 +64,6 @@ export class MapContentComponent implements OnInit {
         });
 
       });
-
   }
 
   public clearCanvas(): void {
@@ -76,86 +75,86 @@ export class MapContentComponent implements OnInit {
   };
 
   public drawLabels(): void {
-      console.log('Drawing some labels');
-      this.mapApiWrapper.getNativeMap()
-          .then((map)=> {
+    console.log('Drawing some labels');
+    this.mapApiWrapper.getNativeMap()
+      .then((map)=> {
 
-              let delta = function (a, b) { return a - b };
+        let delta = function (a, b) { return a - b };
 
-              let geoBounds = undefined;
+        let geoBounds = undefined;
 
-              let bounds = map.getBounds();
+        let bounds = map.getBounds();
 
-              if( bounds ) {
-                  let sw = bounds.getSouthWest();
-                  let ne = bounds.getNorthEast();
-                  geoBounds = {
-                      top:    sw.lat().valueOf(),
-                      bottom: ne.lat().valueOf(),
-                      left:   ne.lng().valueOf(),
-                      right:  sw.lng().valueOf(),
-                      width:  delta(ne.lng().valueOf(), sw.lng().valueOf()),
-                      height: delta(sw.lat().valueOf(), ne.lat().valueOf())
-                  };
-              }
+        if( bounds ) {
+          let sw = bounds.getSouthWest();
+          let ne = bounds.getNorthEast();
+          geoBounds = {
+            top:    sw.lat().valueOf(),
+            bottom: ne.lat().valueOf(),
+            left:   ne.lng().valueOf(),
+            right:  sw.lng().valueOf(),
+            width:  delta(ne.lng().valueOf(), sw.lng().valueOf()),
+            height: delta(sw.lat().valueOf(), ne.lat().valueOf())
+          };
+        }
 
-              let googleMapContent = document.getElementsByClassName("sebm-google-map-content")[0];
-              let markerList = googleMapContent.getElementsByTagName("sebm-google-map-marker");
+        let googleMapContent = document.getElementsByClassName("sebm-google-map-content")[0];
+        let markerList = googleMapContent.getElementsByTagName("sebm-google-map-marker");
 
-              let markerArray = [];
+        let markerArray = [];
 
 
-              if( markerList.length > 0 ) {
-                  for (let i = 0; i < markerList.length; i++ ){
-                      let marker = markerList[i];
-                      let markerLabel = marker.getAttribute("ng-reflect-label");
-                      let markerLat = marker.getAttribute("ng-reflect-latitude");
-                      let markerLng = marker.getAttribute("ng-reflect-longitude");
-                      let deltaLeftSideOfMapToMarker = delta(markerLng, geoBounds.right);
-                      let deltaTopOfMapToMarker = delta(markerLat, geoBounds.bottom);
-                      let markerGeoOffsetLatPercentage = deltaLeftSideOfMapToMarker / geoBounds.width;
-                      let markerGeoOffsetLngPercentage = deltaTopOfMapToMarker / geoBounds.height;
+        if( markerList.length > 0 ) {
+          for (let i = 0; i < markerList.length; i++ ){
+            let marker = markerList[i];
+            let markerLabel = marker.getAttribute("ng-reflect-label");
+            let markerLat = marker.getAttribute("ng-reflect-latitude");
+            let markerLng = marker.getAttribute("ng-reflect-longitude");
+            let deltaLeftSideOfMapToMarker = delta(markerLng, geoBounds.right);
+            let deltaTopOfMapToMarker = delta(markerLat, geoBounds.bottom);
+            let markerGeoOffsetLatPercentage = deltaLeftSideOfMapToMarker / geoBounds.width;
+            let markerGeoOffsetLngPercentage = deltaTopOfMapToMarker / geoBounds.height;
 
-                      let markerObj = {
-                          markerLabel: markerLabel,
-                          markerLat: markerLat,
-                          markerLng: markerLng,
-                          markerOffsetX: deltaLeftSideOfMapToMarker,
-                          markerOffsetY: deltaTopOfMapToMarker,
-                          markerGeoOffsetLatPercentage: markerGeoOffsetLatPercentage,
-                          markerGeoOffsetLngPercentage: markerGeoOffsetLngPercentage
-                      };
+            let markerObj = {
+              markerLabel: markerLabel,
+              markerLat: markerLat,
+              markerLng: markerLng,
+              markerOffsetX: deltaLeftSideOfMapToMarker,
+              markerOffsetY: deltaTopOfMapToMarker,
+              markerGeoOffsetLatPercentage: markerGeoOffsetLatPercentage,
+              markerGeoOffsetLngPercentage: markerGeoOffsetLngPercentage
+            };
 
-                      markerArray.push(markerObj);
-                  }
+            markerArray.push(markerObj);
+          }
 
-                  let dataForDrawing = {
-                      geoBounds: geoBounds,
-                      markers: markerArray
-                  };
+          let dataForDrawing = {
+            geoBounds: geoBounds,
+            markers: markerArray
+          };
 
-                  for(let i = 0; i < dataForDrawing.markers.length; i++) {
-                      console.log(
-                          ' geo delta left: ' + dataForDrawing.markers[0].markerOffsetX);
-                      console.log(
-                          ' geo width:  ' + geoBounds.width +
-                          ' (' + geoBounds.left + ', ' + geoBounds.right + ')');
-                      console.log(
-                          ' Lng offset ' + dataForDrawing.markers[i].markerLng +
-                          ' (' + 100.0 * dataForDrawing.markers[i].markerGeoOffsetLngPercentage + '%)');
-                      console.log(
-                          ' geo delta top:  ' + dataForDrawing.markers[0].markerOffsetY);
-                      console.log(
-                          ' geo height: ' + geoBounds.height +
-                          ' (' + geoBounds.top + ', ' + geoBounds.bottom + ')');
-                      console.log(
-                          ' Lat offset ' + dataForDrawing.markers[i].markerLat +
-                          ' (' + 100.0 * dataForDrawing.markers[i].markerGeoOffsetLatPercentage + '%)');
-                  }
+          for(let i = 0; i < dataForDrawing.markers.length; i++) {
+            console.log(
+              ' geo delta left: ' + dataForDrawing.markers[0].markerOffsetX);
+            console.log(
+              ' geo width:  ' + geoBounds.width +
+              ' (' + geoBounds.left + ', ' + geoBounds.right + ')');
+            console.log(
+              ' Lng offset ' + dataForDrawing.markers[i].markerLng +
+              ' (' + 100.0 * dataForDrawing.markers[i].markerGeoOffsetLngPercentage + '%)');
+            console.log(
+              ' geo delta top:  ' + dataForDrawing.markers[0].markerOffsetY);
+            console.log(
+              ' geo height: ' + geoBounds.height +
+              ' (' + geoBounds.top + ', ' + geoBounds.bottom + ')');
+            console.log(
+              ' Lat offset ' + dataForDrawing.markers[i].markerLat +
+              ' (' + 100.0 * dataForDrawing.markers[i].markerGeoOffsetLatPercentage + '%)');
+          }
 
-                  this.mapHlpr.emitDataForDrawing(dataForDrawing);
-             }
-         });
+          this.mapHlpr.emitDataForDrawing(dataForDrawing);
+     }
+    });
   }
 
   public refreshMapSize(coords: GeoCoordinates){
