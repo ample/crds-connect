609c4beff17e5c6dee11326e10b1c4af47af58a5
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-03-21 21:44:54 -0400

    and linting

--- src/app/validators/email-address.validator.ts
+++ src/app/validators/email-address.validator.ts
@@ -3,7 +3,8 @@ import { AbstractControl, Validators } from '@angular/forms';
 export class EmailAddressValidator {
     static validateEmail(control: AbstractControl): { [key: string]: any } {
         //using the pattern from http://emailregex.com/
+        // tslint:disable-next-line:max-line-length
         let pattern: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return pattern.test(control.value) ? null : { "emailFormat": true };
+        return pattern.test(control.value) ? null : { 'emailFormat': true };
     }
 }
