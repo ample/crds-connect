2f85e3b06c4a76c6a0b39f1e4c7ee030ae60139a
dshannon@callibrity.com <dshannon@callibrity.com>
2017-03-28 15:48:46 -0400

    fixed say hi to yourself

--- src/app/components/pin-details/say-hi/say-hi.component.ts
+++ src/app/components/pin-details/say-hi/say-hi.component.ts
@@ -24,6 +24,8 @@ export class SayHiComponent implements OnInit {
   @Input() pin: Pin;
   @Input() isLoggedIn: boolean = false;
 
+  private route: string;
+
   constructor(
     private pinService: PinService,
     private loginRedirectService: LoginRedirectService,
@@ -33,55 +35,56 @@ export class SayHiComponent implements OnInit {
 
 
   ngOnInit() {
-    this.sendSayHi = this.sendSayHi.bind(this);
+    this.getUserDetailsThenSayHi = this.getUserDetailsThenSayHi.bind(this);
   }
 
   public sayHi() {
     if (!this.isLoggedIn) {
-      this.loginRedirectService.redirectToLogin(this.router.routerState.snapshot.url, this.sendSayHi);
+      this.loginRedirectService.redirectToLogin(this.router.routerState.snapshot.url, this.getUserDetailsThenSayHi);
     } else {
-      this.sendSayHi();
+      this.doSayHi();
     }
   }
 
-  public sendSayHi() {
+  private getUserDetailsThenSayHi() {
+    this.session.getUserData().subscribe(
+      ret => {
+        this.user = ret;
+        if (this.session.getContactId() == this.pin.contactId) {
+          if (this.isGathering) {
+            console.log('is gathering and same user');
+            this.router.navigate(['/gathering/' + this.pin.gathering.groupId]);
+          } else {
+            console.log('is person and same user');
+            this.router.navigate(['/person/' + this.pin.participantId]);
+          }
+        } else {
+          this.doSayHi();
+        }
+      },
+      err => {
+
+      }
+    );
+  }
+
+  private doSayHi() {
     let bpd = new BlandPageDetails(
-          'Return to map',
-          '<div class="container"><div class="row text-center"><h3>Host contacted</h3></div></div>',
-          BlandPageType.Text,
-          BlandPageCause.Success,
-          ''
-        );
-    if (!this.user) {
-      this.session.getUserData().subscribe(
-        ret => {
-          this.user = ret;
-          this.pinService.sendHiEmail(this.user, this.pin).subscribe(
-            out => {
-              this.blandPageService.primeAndGo(bpd);
-            },
-            err => {
-              // redirect to error page
-            }
-
-          );
-        },
-        err => {
-
-        }
-      );
-
-    } else {
-      this.pinService.sendHiEmail(this.user, this.pin).subscribe(
-        ret => {
-          this.blandPageService.primeAndGo(bpd);
-        },
-        err => {
-          // redirect to error page
-        }
-
-      );
-    }
+      'Return to map',
+      '<div class="container"><div class="row text-center"><h3>Host contacted</h3></div></div>',
+      BlandPageType.Text,
+      BlandPageCause.Success,
+      ''
+    );
+    this.pinService.sendHiEmail(this.user, this.pin).subscribe(
+      ret => {
+        console.log('said hi');
+        this.blandPageService.primeAndGo(bpd);
+      },
+      err => {
+        console.log('said hi err');
+        // redirect to error page
+      }
+    );
   }
-
 }
