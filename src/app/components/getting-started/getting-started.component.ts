1ad11fdad4580315d267967f7ee5595af0ec2ee4
Sean C Davis <scdavis41@gmail.com>
2017-03-29 14:45:17 -0400

    US7475 - Getting Started Add app page header to getting started

--- src/app/components/getting-started/getting-started.component.ts
+++ src/app/components/getting-started/getting-started.component.ts
@@ -1,14 +1,18 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { StateService } from '../../services/state.service';
+
 @Component({
   templateUrl: 'getting-started.component.html'
 })
 export class GettingStartedComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+             private state: StateService) {}
 
   ngOnInit() {
+    this.state.setPageHeader('Getting Started', '/');
     return true;
   }
 }
