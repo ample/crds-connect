fbc945432ee5128124a5d4e4ed2ed88956d6dcc4
Sean C Davis <scdavis41@gmail.com>
2017-03-30 16:15:57 -0400

    US7498 - Toast Notifications Account for titles

--- src/app/route-guards/getting-started-guard.ts
+++ src/app/route-guards/getting-started-guard.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { ActivatedRouteSnapshot } from '@angular/router';
 
+import { ToastsManager } from 'ng2-toastr';
+
 import { BlandPageService } from '../services/bland-page.service';
 
 @Injectable()
 export class GettingStartedGuard implements CanActivate {
 
   constructor(private blandPageService: BlandPageService,
-              private router: Router) {}
+              private router: Router,
+              private toast: ToastsManager) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
+    this.toast.info('This is a message', 'Title', { toastLife: 10000000 });
     this.blandPageService.setPageHeader('Getting Started', '/');
     if (!this.blandPageService.primed()) {
       this.blandPageService.primeGettingStarted();
