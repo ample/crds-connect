4f01cd8b0b2130a9f9c259332ed385bba19babb1
zarahzachz <sarah.sachs@gmail.com>
2017-03-31 15:43:27 -0400

    US7473 - Bland page layout Remove debugger line.

--- src/app/services/pin.service.ts
+++ src/app/services/pin.service.ts
@@ -142,7 +142,6 @@ export class PinService extends SmartCacheableService<PinSearchResultsDto, Searc
 
   // POSTS
   public sendHiEmail(user: User, pin: Pin): Observable<any> {
-    debugger;
     // Create merge data for this template
     let emailInfo = {
       'fromEmailAddress': user.email,
@@ -199,7 +198,7 @@ export class PinService extends SmartCacheableService<PinSearchResultsDto, Searc
       })
       .catch((err) => Observable.throw(err.json().error));
   }
-  
+
   public doesLoggedInUserOwnPin(pin: Pin) {
     let contactId = this.session.getContactId();
     return contactId === pin.contactId;
