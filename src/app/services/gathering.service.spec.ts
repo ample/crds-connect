3aaa7b71cb64464c30d2293b38a38c8242e811e8
vchtelmakh <vchtelmakh@callibrity.com>
2017-03-31 14:14:53 -0400

    Add basic unit test

--- src/app/services/gathering.service.spec.ts
+++ src/app/services/gathering.service.spec.ts
@@ -8,15 +8,19 @@ import { Pin, pinType } from '../models/pin';
 import { PinSearchResultsDto } from '../models/pin-search-results-dto';
 import { UserDataForPinCreation } from '../models/user-data-for-pin-creation';
 
+import { MockTestData } from '../shared/MockTestData';
+
 describe('Service: Gathering', () => {
 
+  const oakleyStreetAddress: string = '3500 Madison Road';
   const mockAddress = new Address(123, 'Test St', null, 'TesVille', 'ZZ', '12345', 0, 0, 'US', 'County');
   const mockUserData = new UserDataForPinCreation(111, 222, 9999, 'Bob', 'Smith', 'bobby@bob.com', mockAddress);
   const mockForm = new AddMeToMapFormFields('Test3 St', '', 'KarmaSt', 'II', '54321', true);
   const mockModifiedAddress = new Address(mockAddress.addressId, mockForm.addressLine1, mockForm.addressLine2,
     mockForm.city, mockForm.state, mockForm.zip, 0, 0, 'US', 'County');
 
-  const mockPin = new Pin('Bob', 'Smith', 'bobby@bob.com', 111, 222, mockModifiedAddress, 0, null, 9999, true, '', pinType.PERSON, 0);
+  const mockSitePin = new Pin('Bob', 'Smith', 'bobby@bob.com', 111, 222, mockModifiedAddress, 0, null, 9999, true, '', pinType.SITE, 0);
+  const mockPersonPin = new Pin('Bob', 'Smith', 'bobby@bob.com', 111, 222, mockModifiedAddress, 0, null, 9999, true, '', pinType.PERSON, 0);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -30,5 +34,14 @@ describe('Service: Gathering', () => {
   it('should create a service instance', inject([GatheringService], (service: any) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should add address line 1 to a pin', inject([GatheringService], (service: any) => {
+    let sitePin: Pin = mockSitePin;
+    sitePin.siteName = 'Oakley';
+    let changedSitePin: Pin = service.addAddressToGatheringPin(sitePin);
+    expect(changedSitePin.address.addressLine1).toEqual(oakleyStreetAddress);
+  }));
+
+
 
 });
