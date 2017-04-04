03bb51fdbaa1e58543b04848399f6a46f0ace8ec
Ken Baum <ken.baum@ingagepartners.com>
2017-04-04 11:55:14 -0400

    US7426 refactor add-me-to-component tests

--- src/app/components/add-me-to-map/add-me-to-map.component.spec.ts
+++ src/app/components/add-me-to-map/add-me-to-map.component.spec.ts
@@ -12,7 +12,6 @@ import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AddMeToTheMapHelperService } from '../../services/add-me-to-map-helper.service';
 import { AddressFormComponent } from '../address-form/address-form.component';
 import { GatheringService } from '../../services/gathering.service';
-import { LocationService } from '../../services/location.service';
 import { LoginRedirectService } from '../../services/login-redirect.service';
 import { AddMeToMapComponent } from './add-me-to-map.component';
 import { SessionService } from '../../services/session.service';
@@ -25,10 +24,29 @@ import { ContentService } from 'crds-ng2-content-block/src/content-block/content
 
 describe('Component: Add Me to the Map', () => {
 
-  let mockContentService;
+  let mockAddMeToTheMapHelperService,
+    mockBlandPageService,
+    mockCookieService,
+    mockGatheringService,
+    mockPinService,
+    mockLoginRedirectService,
+    mockSessionService,
+    mockStateService,
+    mockContentService,
+    mockGoogleMapService;
 
   beforeEach(() => {
-    mockContentService = jasmine.createSpyObj<ContentService>('content', ['loadData']);
+
+    mockAddMeToTheMapHelperService = jasmine.createSpyObj<AddMeToTheMapHelperService>('AddMeToTheMapHelperService', ['constructor']);
+    mockBlandPageService = jasmine.createSpyObj<BlandPageService>('BlandPageService', ['constructor']);
+    mockCookieService = jasmine.createSpyObj<CookieService>('cookieService', ['constructor']);
+    mockGatheringService = jasmine.createSpyObj<GatheringService>('gatheringService', ['constructor']);
+    mockPinService = jasmine.createSpyObj<PinService>('pinService', ['constructor']);
+    mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService', ['constructor']);
+    mockSessionService = jasmine.createSpyObj<SessionService>('sessionService', ['constructor']);
+    mockStateService = jasmine.createSpyObj<StateService>('stateService', ['constructor']);
+    mockContentService = jasmine.createSpyObj<ContentService>('contentService', ['loadData']);
+    mockGoogleMapService = jasmine.createSpyObj<GoogleMapService>('mapService', ['constructor']);
 
     TestBed.configureTestingModule({
       declarations: [
@@ -45,18 +63,16 @@ describe('Component: Add Me to the Map', () => {
         ContentBlockModule.forRoot({ categories: ['common'] })
       ],
       providers: [
-        AddMeToTheMapHelperService,
-        BlandPageService,
-        CookieService,
+        { provide: AddMeToTheMapHelperService, useValue: mockAddMeToTheMapHelperService },
+        { provide: BlandPageService, useValue: mockBlandPageService },
+        { provide: CookieService, useValue: mockCookieService },
+        { provide: GatheringService, useValue: mockGatheringService },
+        { provide: PinService, useValue: mockPinService },
+        { provide: LoginRedirectService, useValue: mockLoginRedirectService },
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: StateService, useValue: mockStateService },
         { provide: ContentService, useValue: mockContentService },
-        GatheringService,
-        PinService,
-        LocationService,
-        LoginRedirectService,
-        SessionService,
-        StateService,
-        GoogleMapService,
-        BlandPageService
+        { provide: GoogleMapService, useValue: mockGoogleMapService }
       ]
     });
     this.fixture = TestBed.createComponent(AddMeToMapComponent);
@@ -64,7 +80,7 @@ describe('Component: Add Me to the Map', () => {
 
   });
 
-  it('should create an instance', () => {
+  fit('should create an instance', () => {
     expect(this.component).toBeTruthy();
   });
 
