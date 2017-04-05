1be264cc689257b08afcb070a1a34917cf19ddbd
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-04-05 11:28:51 -0400

    WIP profile pictures.

--- src/app/components/profile-picture/profile-picture.component.ts
+++ src/app/components/profile-picture/profile-picture.component.ts
@@ -1,31 +1,29 @@
-import { ImageService } from '../services/image.service';
-import { Angulartics2 } from 'angulartics2';
 import { Component, OnInit, Input } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
 
 
 
 @Component({
   selector: 'profile-picture',
-  templateUrl: 'profile-picture.html'
+  templateUrl: 'profile-picture.html',
+  styleUrls: ['profile-picture.scss']
 })
 export class ProfilePictureComponent implements OnInit {
 
   @Input() contactId: number;
   @Input() wrapperClass: string = '';
   @Input() imageClass: string = '';
-  public path: string;
+  private path: string;
+  private baseUrl: string = process.env.CRDS_API_ENDPOINT;
+  private DefaultProfileImage: string = '//crossroads-media.imgix.net/images/avatar.svg';
+  private ImageBaseURL: string = `${this.baseUrl}api/v1.0.0/image/profile/`;
 
 
-  constructor(private imageService: ImageService
-              ) {
+  constructor() {
 
   }
 
   public ngOnInit() {
-        this.path = this.imageService.ImageBaseURL + this.contactId;
+        this.path = this.ImageBaseURL + this.contactId;
   }
-
-
 }
