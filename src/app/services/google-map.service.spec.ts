fc103faacf0dfbefdba00d12066a836dd2ab2ac4
vchtelmakh <vchtelmakh@callibrity.com>
2017-04-11 11:25:04 -0400

    Fix broken spec file to begin adding tests

--- src/app/services/google-map.service.spec.ts
+++ src/app/services/google-map.service.spec.ts
@@ -1,28 +1,40 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Http, Response, RequestOptions } from '@angular/http';
-import { AgmCoreModule } from 'angular2-google-maps/core';
-import { UserLocationService } from './/user-location.service';
+
+import { AgmCoreModule, GoogleMapsAPIWrapper } from 'angular2-google-maps/core';
+import { CanvasMapOverlayComponent } from '../components/canvas-map-overlay/canvas-map-overlay.component';
 import { MapComponent } from '../components/map/map.component';
 import { MapContentComponent } from '../components/map-content/map-content.component';
 import { MapFooterComponent } from '../components/map-footer/map-footer.component';
-import { GeoCoordinates } from '../models/geo-coordinates';
+import { SearchLocalComponent } from '../components/search-local/search-local.component';
 
+import { BlandPageService } from '../services/bland-page.service';
 import { IFrameParentService } from './/iframe-parent.service';
+import { IPService } from '../services/ip.service';
+import { NeighborsHelperService } from '../services/neighbors-helper.service';
+import { SearchService } from '../services/search.service';
 import { SessionService } from './/session.service';
 import { StateService } from './/state.service';
 import { StoreService } from './/store.service';
+import { SiteAddressService } from '../services/site-address.service';
 import { GoogleMapService } from './/google-map.service';
 import { LoginRedirectService } from './/login-redirect.service';
 import { Angulartics2 } from 'angulartics2';
 import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
+import { PinLabelService } from '../services/pin-label.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule, JsonpModule  } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { LocationService } from './/location.service';
 import { PinService}  from './/pin.service';
+import { UserLocationService } from './/user-location.service';
+
+import { GeoCoordinates } from '../models/geo-coordinates';
+
 import { GoogleMapClusterDirective } from  '../directives/google-map-cluster.directive';
 
 describe('Service: Google Map', () => {
@@ -30,9 +42,11 @@ describe('Service: Google Map', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
+        CanvasMapOverlayComponent,
         MapComponent,
         MapContentComponent,
         MapFooterComponent,
+        SearchLocalComponent,
         GoogleMapClusterDirective
       ],
       imports: [
@@ -42,11 +56,18 @@ describe('Service: Google Map', () => {
         })
       ],
       providers: [
+        BlandPageService,
         UserLocationService,
+        IPService,
         LocationService,
         PinService,
         GoogleMapService,
+        GoogleMapsAPIWrapper,
         IFrameParentService,
+        NeighborsHelperService,
+        PinLabelService,
+        SearchService,
+        SiteAddressService,
         StoreService,
         StateService,
         SessionService,
@@ -60,6 +81,9 @@ describe('Service: Google Map', () => {
 
   });
 
+  it('should create an instance', inject([PinService], (service: PinService) => {
+    expect(service).toBeTruthy();
+  }));
 
   xit('should emit geo-coordinates', () => {
 
