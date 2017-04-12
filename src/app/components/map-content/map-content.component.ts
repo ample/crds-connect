1e1523f1d66fc8b86b81469c1fb68a32c224dafb
vchtelmakh <vchtelmakh@callibrity.com>
2017-04-12 16:07:15 -0400

    Move out constant into constants file

--- src/app/components/map-content/map-content.component.ts
+++ src/app/components/map-content/map-content.component.ts
@@ -6,92 +6,7 @@ import { StateService } from '../../services/state.service';
 import { GeoCoordinates } from '../../models/geo-coordinates';
 import { MapView } from '../../models/map-view';
 
-// /** @constructor */
-// function USGSOverlay(bounds, image, map) {
-//
-//   console.log('Running constructor');
-//   // Initialize all properties.
-//   this.bounds_ = bounds;
-//   this.image_ = image;
-//   this.map_ = map;
-//
-//   // Define a property to hold the image's div. We'll
-//   // actually create this div upon receipt of the onAdd()
-//   // method so we'll leave it null for now.
-//   this.div_ = null;
-//
-//   // Explicitly call setMap on this overlay.
-//   this.setMap(map);
-//   console.log('Constructor done');
-// }
-//
-// /**
-//  * onAdd is called when the map's panes are ready and the overlay has been
-//  * added to the map.
-//  */
-// USGSOverlay.prototype.onAdd = function() {
-//
-//   console.log('Running on add');
-//
-//   var div = document.createElement('div');
-//   div.style.borderStyle = 'none';
-//   div.style.borderWidth = '0px';
-//   div.style.position = 'absolute';
-//
-//   // Create the img element and attach it to the div.
-//   var img = document.createElement('img');
-//   img.src = this.image_;
-//   img.style.width = '100%';
-//   img.style.height = '100%';
-//   img.style.position = 'absolute';
-//   div.appendChild(img);
-//
-//   this.div_ = div;
-//
-//   // Add the element to the "overlayLayer" pane.
-//   var panes = this.getPanes();
-//   panes.overlayLayer.appendChild(div);
-//
-//   console.log('Done with on add');
-// };
-//
-// USGSOverlay.prototype.draw = function() {
-//
-//   // // We use the south-west and north-east
-//   // // coordinates of the overlay to peg it to the correct position and size.
-//   // // To do this, we need to retrieve the projection from the overlay.
-//   // var overlayProjection = this.getProjection();
-//   //
-//   // // Retrieve the south-west and north-east coordinates of this overlay
-//   // // in LatLngs and convert them to pixel coordinates.
-//   // // We'll use these coordinates to resize the div.
-//   // var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-//   // var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-//   //
-//   // // Resize the image's div to fit the indicated dimensions.
-//   // var div = this.div_;
-//   // div.style.left = sw.x + 'px';
-//   // div.style.top = ne.y + 'px';
-//   // div.style.width = (ne.x - sw.x) + 'px';
-//   // div.style.height = (sw.y - ne.y) + 'px';
-//
-//   //----
-//   var overlayProjection = this.getProjection();
-//   var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-//   var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-//   var div = this.div_;
-//   div.style.left = sw.x + 'px';
-//   div.style.top = ne.y + 'px';
-//   div.style.width = (ne.x - sw.x) + 'px';
-//   div.style.height = (sw.y - ne.y) + 'px';
-// };
-//
-// // The onRemove() method will be called automatically from the API if
-// // we ever set the overlay's map property to 'null'.
-// USGSOverlay.prototype.onRemove = function() {
-//   this.div_.parentNode.removeChild(this.div_);
-//   this.div_ = null;
-// };
+import { googleMapStyles } from '../../shared/constants';
 
 // This does need to be 'declare'
 declare let google: any;
@@ -138,166 +53,7 @@ export class MapContentComponent implements OnInit {
           minZoom: 3,
           maxZoom: 20,
           scrollwheel: false,
-          styles: [
-            {
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#f5f5f5'
-                }
-              ]
-            },
-            {
-              'elementType': 'labels.icon',
-              'stylers': [
-                {
-                  'visibility': 'off'
-                }
-              ]
-            },
-            {
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#616161'
-                }
-              ]
-            },
-            {
-              'elementType': 'labels.text.stroke',
-              'stylers': [
-                {
-                  'color': '#f5f5f5'
-                }
-              ]
-            },
-            {
-              'featureType': 'administrative.land_parcel',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#bdbdbd'
-                }
-              ]
-            },
-            {
-              'featureType': 'poi',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#eeeeee'
-                }
-              ]
-            },
-            {
-              'featureType': 'poi',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#757575'
-                }
-              ]
-            },
-            {
-              'featureType': 'poi.park',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#e5e5e5'
-                }
-              ]
-            },
-            {
-              'featureType': 'poi.park',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#9e9e9e'
-                }
-              ]
-            },
-            {
-              'featureType': 'road',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#ffffff'
-                }
-              ]
-            },
-            {
-              'featureType': 'road.arterial',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#757575'
-                }
-              ]
-            },
-            {
-              'featureType': 'road.highway',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#dadada'
-                }
-              ]
-            },
-            {
-              'featureType': 'road.highway',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#616161'
-                }
-              ]
-            },
-            {
-              'featureType': 'road.local',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#9e9e9e'
-                }
-              ]
-            },
-            {
-              'featureType': 'transit.line',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#e5e5e5'
-                }
-              ]
-            },
-            {
-              'featureType': 'transit.station',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#eeeeee'
-                }
-              ]
-            },
-            {
-              'featureType': 'water',
-              'elementType': 'geometry',
-              'stylers': [
-                {
-                  'color': '#c9c9c9'
-                }
-              ]
-            },
-            {
-              'featureType': 'water',
-              'elementType': 'labels.text.fill',
-              'stylers': [
-                {
-                  'color': '#9e9e9e'
-                }
-              ]
-            }
-          ]
+          styles: googleMapStyles
         });
 
         let overlay;
@@ -340,17 +96,17 @@ export class MapContentComponent implements OnInit {
           self.state.setMapView(mapViewUpdate);
 
           console.log('Drawing overlay');
-          overlay = new USGSOverlay(bounds, srcImage, map);
+          overlay = new USGSOverlay(bounds, srcImage, map, 'label');
+          overlay = new USGSOverlay(bounds, 'http://inspectiondoc.com/wp-content/uploads/2014/08/sample-icon.png', map, 'label');
         });
 
         /** @constructor */
-        function USGSOverlay(bounds, image, map) {
-
-          console.log('Running constructor');
+        function USGSOverlay(bounds, image, map, label) {
           // Initialize all properties.
           this.bounds_ = bounds;
           this.image_ = image;
           this.map_ = map;
+          this.label_ = label;
 
           // Define a property to hold the image's div. We'll
           // actually create this div upon receipt of the onAdd()
@@ -359,7 +115,6 @@ export class MapContentComponent implements OnInit {
 
           // Explicitly call setMap on this overlay.
           this.setMap(map);
-          console.log('Constructor done');
         }
 
         /**
@@ -367,8 +122,6 @@ export class MapContentComponent implements OnInit {
          * added to the map.
          */
         USGSOverlay.prototype.onAdd = function() {
-
-          console.log('Running on add');
 
           var div = document.createElement('div');
           div.style.borderStyle = 'none';
@@ -388,31 +141,9 @@ export class MapContentComponent implements OnInit {
           // Add the element to the "overlayLayer" pane.
           var panes = this.getPanes();
           panes.overlayLayer.appendChild(div);
-
-          console.log('Done with on add');
         };
 
         USGSOverlay.prototype.draw = function() {
-
-          // // We use the south-west and north-east
-          // // coordinates of the overlay to peg it to the correct position and size.
-          // // To do this, we need to retrieve the projection from the overlay.
-          // var overlayProjection = this.getProjection();
-          //
-          // // Retrieve the south-west and north-east coordinates of this overlay
-          // // in LatLngs and convert them to pixel coordinates.
-          // // We'll use these coordinates to resize the div.
-          // var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-          // var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-          //
-          // // Resize the image's div to fit the indicated dimensions.
-          // var div = this.div_;
-          // div.style.left = sw.x + 'px';
-          // div.style.top = ne.y + 'px';
-          // div.style.width = (ne.x - sw.x) + 'px';
-          // div.style.height = (sw.y - ne.y) + 'px';
-
-          //----
           var overlayProjection = this.getProjection();
           var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
           var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
