1c284a78a81ad4eea0a274d85b9869775249dcaa
vchtelmakh <vchtelmakh@callibrity.com>
2017-04-20 14:35:19 -0400

    Add form to dto conversion in helper service

--- src/app/components/host-application/host-application.component.ts
+++ src/app/components/host-application/host-application.component.ts
@@ -6,12 +6,14 @@ import { Angulartics2 } from 'angulartics2';
 import { ToastsManager } from 'ng2-toastr';
 
 import { BlandPageService } from '../../services/bland-page.service';
+import { HostApplicationHelperService } from '../../services/host-application-helper.service';
 import { LoginRedirectService } from '../../services/login-redirect.service';
 import { SessionService } from '../../services/session.service';
 import { StateService } from '../../services/state.service';
 import { StoreService } from '../../services/store.service';
 
 import { Address } from '../../models/address';
+import { HostRequestDto } from '../../models/host-request-dto';
 import { HostApplicatonForm } from '../../models/host-application-form';
 import { DetailedUserData } from '../../models/detailed-user-data';
 
@@ -30,6 +32,7 @@ export class HostApplicationComponent implements OnInit {
 
   constructor(
     private blandPageService: BlandPageService,
+    private hlpr: HostApplicationHelperService,
     private loginRedirectService: LoginRedirectService,
     private route: ActivatedRoute,
     private router: Router,
@@ -57,9 +60,12 @@ export class HostApplicationComponent implements OnInit {
 
     this.isFormSubmitted = true;
 
+    let dto: HostRequestDto = this.hlpr.convertFormToDto(value, this.userData.contactId);
+
     this.toast.error('Address form submitted! Testing toaster.');
     console.log('Address form submitted');
     console.log(value);
+    console.log(dto);
 
   }
 
--- src/app/services/host-application-helper.service.ts
+++ src/app/services/host-application-helper.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Address } from '../models/address';
 import { AddMeToMapFormFields } from '../models/add-me-to-map-form-fields';
+import { HostRequestDto } from '../models/host-request-dto';
+import { HostApplicatonForm } from '../models/host-application-form';
 import { LookupTable } from '../models/lookup-table';
 import { Pin, pinType } from '../models/pin';
 import { UserDataForPinCreation } from '../models/user-data-for-pin-creation';
@@ -9,7 +11,19 @@ import { UserDataForPinCreation } from '../models/user-data-for-pin-creation';
 @Injectable()
 export class HostApplicationHelperService {
 
-
   constructor() { }
+
+  public convertFormToDto(hostForm: HostApplicatonForm, contactId: number): HostRequestDto {
+
+    let dto: HostRequestDto = new HostRequestDto(
+      contactId,
+      hostForm.isHomeAddress ? hostForm.homeAddress : hostForm.groupAddress,
+      hostForm.isHomeAddress,
+      hostForm.contactNumber,
+      hostForm.groupDescription
+    );
+
+    return dto;
+  };
 
 }
