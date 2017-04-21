53d2930f06709f167d58f4902b8e1003c846533b
Ken Baum <ken.baum@ingagepartners.com>
2017-04-21 11:07:37 -0400

    US7426 map footer

--- src/app/components/map-footer/map-footer.component.spec.ts
+++ src/app/components/map-footer/map-footer.component.spec.ts
@@ -1,16 +1,11 @@
 /* tslint:disable:no-unused-variable */
 import { Angulartics2 } from 'angulartics2';
 import { NO_ERRORS_SCHEMA, EventEmitter } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { Router } from '@angular/router';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { MockTestData } from '../../shared/MockTestData';
 import { Observable } from 'rxjs/Rx';
-
-import { ListHelperService } from '../../services/list-helper.service';
 import { MapFooterComponent } from './map-footer.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { SiteAddressService } from '../../services/site-address.service';
 import { NeighborsHelperService } from '../../services/neighbors-helper.service';
 import { PinService } from '../../services/pin.service';
 import { SearchService } from '../../services/search.service';
@@ -18,11 +13,8 @@ import { SessionService } from '../../services/session.service';
 import { StateService } from '../../services/state.service';
 import { BlandPageService } from '../../services/bland-page.service';
 import { LoginRedirectService } from '../../services/login-redirect.service';
-import { GoogleMapService } from '../../services/google-map.service';
 import { UserLocationService } from  '../../services/user-location.service';
-import { GeoCoordinates } from '../../models/geo-coordinates';
-import { Pin, pinType } from '../../models/pin';
-import { PinSearchResultsDto } from '../../models/pin-search-results-dto';
+
 
 
 describe('Component: MapFooter', () => {
@@ -37,17 +29,17 @@ describe('Component: MapFooter', () => {
     let mockBlandPageService;
     let mockUserLocationService;
     let mockAngulartics2;
+    let mockSearchService;
 
     beforeEach(() => {
         mockPinService = jasmine.createSpyObj<PinService>('pinService', ['getPinSearchResults']);
         mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService', ['redirectToLogin']);
-        mockStateService = jasmine.createSpyObj<StateService>(
-            'state', ['setLoading', 'setPageHeader', 'setCurrentView', 'setMyViewOrWorldView', 'getCurrentView']);
+        mockStateService = jasmine.createSpyObj<StateService>('state', ['setLoading', 'setPageHeader', 'setCurrentView', 'setMyViewOrWorldView', 'getCurrentView']);
         mockSessionService = jasmine.createSpyObj<SessionService>('session', ['getContactId', 'isLoggedIn']);
         mockBlandPageService = jasmine.createSpyObj<BlandPageService>('blandPageService', ['primeAndGo', 'goToDefaultError']);
-        mockUserLocationService = jasmine.createSpyObj<UserLocationService>(
-            'userLocationService', ['setLoading', 'setPageHeader', 'GetUserLocation']);
+        mockUserLocationService = jasmine.createSpyObj<UserLocationService>('userLocationService', ['setLoading', 'setPageHeader', 'GetUserLocation']);
         mockAngulartics2 = jasmine.createSpyObj<Angulartics2>('angulartics2', ['eventTrack']);
+        mockSearchService = jasmine.createSpyObj<SearchService>('searchService',['constructor']);
 
         TestBed.configureTestingModule({
             declarations: [
@@ -60,8 +52,8 @@ describe('Component: MapFooter', () => {
                 { provide: SessionService, useValue: mockSessionService },
                 { provide: BlandPageService, useValue: mockBlandPageService },
                 { provide: UserLocationService, useValue: mockUserLocationService },
-                SearchService,
-                { provide: Angulartics2, useValue: mockAngulartics2}
+                { provide: Angulartics2, useValue: mockAngulartics2 },
+                { provide: SearchService, useValue: mockSearchService }
             ],
             imports: [
                 RouterTestingModule.withRoutes([])
