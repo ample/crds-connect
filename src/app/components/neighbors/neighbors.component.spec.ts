7cf3feb24ffe29d4c48bccfcfbe7d5313d63c3cd
Ken Baum <ken.baum@ingagepartners.com>
2017-04-21 14:42:28 -0400

    US7426 neighbors

--- src/app/components/neighbors/neighbors.component.spec.ts
+++ src/app/components/neighbors/neighbors.component.spec.ts
@@ -2,6 +2,8 @@
 
 import { TestBed } from '@angular/core/testing';
 import { Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from '@angular-cli/ast-tools/node_modules/rxjs/Rx';
+import { MapView } from '../../models/map-view';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { UserLocationService } from '../../services/user-location.service';
 import { NeighborsComponent } from './neighbors.component';
@@ -12,90 +14,100 @@ import { SearchBarComponent } from '../../components/search-bar/search-bar.compo
 import { SearchLocalComponent } from '../../components/search-local/search-local.component';
 import { MapContentComponent } from '../../components/map-content/map-content.component';
 import { MapFooterComponent } from '../map-footer/map-footer.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ContentBlockModule } from 'crds-ng2-content-block';
-import { ContentService } from 'crds-ng2-content-block/src/content-block/content.service';
 import { SiteAddressService } from '../../services/site-address.service';
-import { IFrameParentService } from '../../services/iframe-parent.service';
 import { SessionService } from '../../services/session.service';
 import { GoogleMapService } from '../../services/google-map.service';
 import { NeighborsHelperService } from '../../services/neighbors-helper.service';
 import { StateService } from '../../services/state.service';
-import { StoreService } from '../../services/store.service';
 import { SearchService } from '../../services/search.service';
-import { ListHelperService } from '../../services/list-helper.service';
 import { ListFooterComponent } from '../../components/list-footer/list-footer.component';
 import { LoginRedirectService } from '../../services/login-redirect.service';
-import { Angulartics2 } from 'angulartics2';
 import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpModule, JsonpModule  } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { LocationService } from '../../services/location.service';
-import { PinService}  from '../../services/pin.service';
-import { GoogleMapClusterDirective } from  '../../directives/google-map-cluster.directive';
+import { PinService } from '../../services/pin.service';
+import { GoogleMapClusterDirective } from '../../directives/google-map-cluster.directive';
 import { BlandPageService } from '../../services/bland-page.service';
 import { GeoCoordinates } from '../../models/geo-coordinates';
 import { Pin } from '../../models/pin';
 import { PinSearchResultsDto } from '../../models/pin-search-results-dto';
 import { IPService } from '../../services/ip.service';
 import { MockComponent } from '../../shared/mock.component';
-import { AddressService}  from '../../services/address.service';
+import { AddressService } from '../../services/address.service';
 
 describe('Component: Neighbors', () => {
-
-  let mockContentService;
+  let mockSiteAddressService,
+    mockUserLocationService,
+    mockLocationService,
+    mockPinService,
+    mockGoogleMapService,
+    mockNeighborsHelperService,
+    mockStateService,
+    mockSearchService,
+    mockSessionService,
+    mockCookieService,
+    mockLoginRedirectService,
+    mockBlandPageService,
+    mockIPService,
+    mockAddressService;
 
   beforeEach(() => {
-    mockContentService = jasmine.createSpyObj<ContentService>('content', ['loadData']);
+
+    mockSiteAddressService = jasmine.createSpyObj<SiteAddressService>('siteAddressService', ['']);
+    mockUserLocationService = jasmine.createSpyObj<UserLocationService>('userLocationService', ['GetUserLocation']);
+    mockLocationService = jasmine.createSpyObj<LocationService>('locationService', ['getCurrentPosition']);
+    mockPinService = jasmine.createSpyObj<PinService>('pinService', ['getPinSearchResults']);
+    mockGoogleMapService = jasmine.createSpyObj<GoogleMapService>('googleMapService', ['constructor', 'setDidUserAllowGeoLoc']);
+    mockNeighborsHelperService = jasmine.createSpyObj<NeighborsHelperService>('neighborsHelperService', ['']);
+    mockStateService = jasmine.createSpyObj<StateService>('stateService', ['setUseZoom', 'setLoading', 'getMyViewOrWorldView', 'getCurrentView', 'getLastSearch']);
+    mockSearchService = jasmine.createSpyObj<SearchService>('searchService', ['']);
+    mockSessionService = jasmine.createSpyObj<SessionService>('sessionService', ['getContactId', 'get', 'isLoggedIn']);
+    mockCookieService = jasmine.createSpyObj<CookieService>('cookieService', ['']);
+    mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService', ['']);
+    mockBlandPageService = jasmine.createSpyObj<BlandPageService>('blandPageService', ['']);
+    mockIPService = jasmine.createSpyObj<IPService>('ipService', ['']);
+    mockAddressService = jasmine.createSpyObj<AddressService>('addressService', ['']);
 
     TestBed.configureTestingModule({
       declarations: [
         NeighborsComponent,
-        MapContentComponent,
-        MapFooterComponent,
-        ListFooterComponent,
-        ListViewComponent,
-        ListEntryComponent,
-        SearchBarComponent,
-        MapComponent,
-        SearchLocalComponent,
-        GoogleMapClusterDirective,
+        MockComponent({ selector: 'app-listview', inputs: ['searchResults']}),
+        MockComponent({ selector: 'app-map', inputs: ['searchResults']}),
+        MockComponent({ selector: 'app-search-bar', inputs:  ['isMapHidden']}),
         MockComponent({ selector: 'profile-picture', inputs: ['contactId', 'wrapperClass', 'imageClass'] }),
-        MockComponent({ selector: 'crds-content-block', inputs: ['id']})
+        MockComponent({ selector: 'crds-content-block', inputs: ['id'] })
       ],
       imports: [
-        RouterTestingModule.withRoutes([]), HttpModule, JsonpModule, ReactiveFormsModule, AlertModule, FormsModule,
+        RouterTestingModule.withRoutes([]), HttpModule, ReactiveFormsModule, FormsModule,
         AgmCoreModule.forRoot({
           apiKey: 'AIzaSyArKsBK97N0Wi-69x10OL7Sx57Fwlmu6Cs'
         })
       ],
       providers: [
-        SiteAddressService,
+        // These services could not be mocked and have the tests pass. The ngOnInit needs
+        // major refactoring to make it testable. Sad.
         UserLocationService,
         LocationService,
         PinService,
-        IFrameParentService,
-        GoogleMapService,
-        NeighborsHelperService,
-        StoreService,
-        StateService,
         SearchService,
-        ListHelperService,
         SessionService,
         CookieService,
-        Angulartics2,
-        { provide: ContentService, useValue: mockContentService },
-        LoginRedirectService,
-        BlandPageService,
-        IPService,
-        AddressService
+        { provide: SiteAddressService, useValue: mockSiteAddressService },
+        { provide: GoogleMapService, useValue: mockGoogleMapService },
+        { provide: NeighborsHelperService, useValue: mockNeighborsHelperService },
+        { provide: StateService, useValue: mockStateService },
+        { provide: LoginRedirectService, useValue: mockLoginRedirectService },
+        { provide: BlandPageService, useValue: mockBlandPageService },
+        { provide: IPService, useValue: mockIPService },
+        { provide: AddressService, useValue: mockAddressService }
       ]
     });
     this.fixture = TestBed.createComponent(NeighborsComponent);
     this.component = this.fixture.componentInstance;
-
   });
 
   it('should create an instance', () => {
