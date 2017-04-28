5d73260c72fdcba609aaebf9a075e01812409cec
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-04-28 07:36:43 -0400

    Handle errors

--- src/app/route-resolvers/detailed-user-data-resolver.ts
+++ src/app/route-resolvers/detailed-user-data-resolver.ts
@@ -1,19 +1,37 @@
+import { BlandPageService } from '../services/bland-page.service';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import { StateService } from '../services/state.service';
-
+import { BlandPageType, BlandPageDetails, BlandPageCause } from '../models';
 import { SessionService } from '../services/session.service';
 
 @Injectable()
 export class DetailedUserDataResolver implements Resolve<any> {
 
   constructor(private session: SessionService,
-              private state: StateService) { }
+              private state: StateService,
+              private blandPageService: BlandPageService) { }
 
   resolve(): Observable<any> {
     this.state.setLoading(true);
-    return this.session.getDetailedUserData();
+
+    return this.session.getDetailedUserData()
+    .catch((err: Response, caught: Observable<any>) => {
+        if (err !== undefined) {
+          let pinResolverError = new BlandPageDetails(
+            'Back To The Map',
+            'Unable to get user profile data',
+            BlandPageType.Text,
+            BlandPageCause.Error,
+            '',
+            ''
+          );
+          this.blandPageService.primeAndGo(pinResolverError);
+          return Observable.throw('Unable to get user profile data');
+        }
+        return Observable.throw(caught);
+      });
   }
 }
