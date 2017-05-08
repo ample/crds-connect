2ffc37c96c6dce4a5aa001706de4ed84f3ef9313
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-05-08 12:27:49 -0400

    call ngc in package.json

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,50 +1,30 @@
-const ngToolsWebpack = require('@ngtools/webpack');
+var path = require('path');
+
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var commonConfig = require('./webpack.common.js');
-var helpers = require('./helpers');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+var commonConfig = require('./webpack.common');
 
 module.exports = webpackMerge(commonConfig, {
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  entry: './src/main.aot.ts',
-  output: {
-    path: __dirname + '/dist',
-    publicPath: 'dist/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new ngToolsWebpack.AotPlugin({
-      tsConfigPath: './tsconfig-aot.json'
-    }),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true,
-			debug: false
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			output: {
-				comments: false
-			},
-			sourceMap: true
-		})
-  ],
-  module: {
-    loaders: [
-      { test: /\.scss$/, loaders: ['raw-loader', 'sass-loader'] },
-      { test: /\.css$/, loader: 'raw-loader' },
-      { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.ts$/, loader: '@ngtools/webpack' }
+   entry: './src/main-aot.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[hash].js',
+        chunkFilename: '[id].[hash].chunk.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    { loader: 'awesome-typescript-loader'},
+                    { loader: 'angular2-template-loader' },
+                    { loader: 'angular-router-loader?aot=true' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
     ]
-  },
-  devServer: {
-    historyApiFallback: true
-  }
 });
