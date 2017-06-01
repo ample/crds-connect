210124e1b90beba31476a25f603d788b5d4a1da5
vchtelmakh <vchtelmakh@callibrity.com>
2017-06-01 10:22:22 -0400

    Consistent line indentation

--- src/app/services/app-settings.service.ts
+++ src/app/services/app-settings.service.ts
@@ -10,16 +10,16 @@ export class AppSettingsService {
   constructor() {}
 
   setAppSettings(appType: AppType) {
-      switch (appType) {
-          case AppType.Connect:
-            this.finderType = app.CONNECT;
-            this.leadershipApplicationType = LeadershipApplicationType.ANYWHERE_HOST;
-            break;
-          case AppType.Groups:
-            this.finderType = app.SMALL_GROUPS;
-            this.leadershipApplicationType = LeadershipApplicationType.GROUP_LEADER;
-            break;
-      }
+    switch (appType) {
+      case AppType.Connect:
+        this.finderType = app.CONNECT;
+        this.leadershipApplicationType = LeadershipApplicationType.ANYWHERE_HOST;
+        break;
+      case AppType.Groups:
+        this.finderType = app.SMALL_GROUPS;
+        this.leadershipApplicationType = LeadershipApplicationType.GROUP_LEADER;
+        break;
+    }
   }
 
   public isConnectApp(): boolean {
@@ -33,8 +33,8 @@ export class AppSettingsService {
   }
 
   public getBaseUrlForCurrentApp(): string {
-      let baseUrlForApp: string = this.isConnectApp() ? appRoute.CONNECT_ROUTE : appRoute.SMALL_GROUPS_ROUTE;
-      return baseUrlForApp;
+    let baseUrlForApp: string = this.isConnectApp() ? appRoute.CONNECT_ROUTE : appRoute.SMALL_GROUPS_ROUTE;
+    return baseUrlForApp;
   }
 
 }
