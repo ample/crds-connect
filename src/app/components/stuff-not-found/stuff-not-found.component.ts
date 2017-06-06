5f1971fd9a367c59e700dd4229d7d6d7c48581bc
vchtelmakh <vchtelmakh@callibrity.com>
2017-06-06 16:56:48 -0400

    fixie stuffy

--- src/app/components/stuff-not-found/stuff-not-found.component.ts
+++ src/app/components/stuff-not-found/stuff-not-found.component.ts
@@ -4,17 +4,18 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'stuff-not-found',
-  templateUrl: 'stuff-not-found.html'
+    selector: 'stuff-not-found',
+    templateUrl: 'stuff-not-found.html'
 })
 export class StuffNotFoundComponent implements OnInit {
 
 
-  constructor(private state: StateService) { }
+    constructor(private state: StateService) { }
 
-  public ngOnInit() {
-      this.state.setPageHeader('my stuff', '/');
-      this.state.setLoading(false);
-  }
+    public ngOnInit() {
+        this.state.setPageHeader('my stuff', '/');
+        this.state.setLoading(false);
+        this.state.myStuffActive = false;
+    }
 
 }
