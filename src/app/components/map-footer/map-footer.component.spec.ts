8dab314ffc76af3234529e5c860404620c8c220a
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-06-16 14:39:01 -0400

    Map footer tests.

--- src/app/components/map-footer/map-footer.component.spec.ts
+++ src/app/components/map-footer/map-footer.component.spec.ts
@@ -1,77 +1,71 @@
-/* tslint:disable:no-unused-variable */
-import { Angulartics2 } from 'angulartics2';
-import { NO_ERRORS_SCHEMA, EventEmitter } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { Observable } from 'rxjs/Rx';
-import { MapFooterComponent } from './map-footer.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { NeighborsHelperService } from '../../services/neighbors-helper.service';
+import { Angulartics2 } from 'angulartics2/dist';
+
+import { AppSettingsService } from '../../services/app-settings.service';
+import { BlandPageService } from '../../services/bland-page.service';
+import { LoginRedirectService } from '../../services/login-redirect.service';
 import { PinService } from '../../services/pin.service';
 import { SearchService } from '../../services/search.service';
 import { SessionService } from '../../services/session.service';
 import { StateService } from '../../services/state.service';
-import { BlandPageService } from '../../services/bland-page.service';
-import { LoginRedirectService } from '../../services/login-redirect.service';
-import { UserLocationService } from  '../../services/user-location.service';
-import { AppSettingsService } from '../../services/app-settings.service';
+import { UserLocationService } from '../../services/user-location.service';
+import { MapFooterComponent } from './map-footer.component';
+import { PinSearchRequestParams } from '../../models/pin-search-request-params';
 
-function fakenext(param: any) { return 1; }
+describe('MapFooterComponent', () => {
+    class Angulartics2Stub {
+        eventTrack = {
+            next: jasmine.createSpy('next')
+        };
+    };
 
-class MockEventTrack {
-    next = fakenext;
-}
-
-class MockAngulartic {
-    eventTrack = new MockEventTrack();
-};
-
-
-describe('Component: MapFooter', () => {
+    class StateStub {
+        setCurrentView = jasmine.createSpy('setCurrentView');
+        setLoading = jasmine.createSpy('setLoading');
+        setMyViewOrWorldView = jasmine.createSpy('setMyViewOrWorldView');
+        setIsMyStuffActive = jasmine.createSpy('setIsMyStuffActive');
+        lastSearch = { search: null };
+        myStuffActive = false;
+    }
     let fixture: ComponentFixture<MapFooterComponent>;
     let comp: MapFooterComponent;
-    let mockPinService;
-    let mockLoginRedirectService;
-    let mockNeighborsHelperService;
-    let mockRouter;
-    let mockStateService;
-    let mockSessionService;
-    let mockBlandPageService;
-    let mockUserLocationService;
-    let mockAngulartics2;
-    let mockSearchService;
-    let mockAppSettingsService;
+    let el;
+    let mockAppSettings, mockPinService, mockLoginRedirectService,
+        mockRouter, mockState, mockSession, mockBlandPageService, mockUserLocationService,
+        mockSearchService, mockAngulartics2;
 
     beforeEach(() => {
-        mockPinService = jasmine.createSpyObj<PinService>('pinService', ['getPinSearchResults']);
-        mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService', ['redirectToLogin']);
-        mockStateService = jasmine.createSpyObj<StateService>('state', ['setLoading', 'setPageHeader', 'setCurrentView', 'setMyViewOrWorldView', 'getCurrentView']);
-        mockSessionService = jasmine.createSpyObj<SessionService>('session', ['getContactId', 'isLoggedIn']);
-        mockBlandPageService = jasmine.createSpyObj<BlandPageService>('blandPageService', ['primeAndGo', 'goToDefaultError']);
-        mockUserLocationService = jasmine.createSpyObj<UserLocationService>('userLocationService', ['setLoading', 'setPageHeader', 'GetUserLocation']);
-        mockAngulartics2 = new MockAngulartic();
-        mockSearchService = jasmine.createSpyObj<SearchService>('searchService',['constructor']);
-        mockAppSettingsService = jasmine.createSpyObj<AppSettingsService>('appSettingService', ['constructor']);
-
-        mockStateService.myStuffActive = false;
+        mockAppSettings = jasmine.createSpyObj<AppSettingsService>('appSettings', ['isConnectApp']);
+        mockPinService = jasmine.createSpyObj<PinService>('pinService', ['emitPinSearchRequest', 'clearPinCache']);
+        mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService',
+            ['redirectToTarget', 'redirectToLogin']);
+        mockState = new StateStub();
+        mockSession = jasmine.createSpyObj<SessionService>('session', ['isLoggedIn']);
+        mockBlandPageService = jasmine.createSpyObj<BlandPageService>('blandPageService', ['goToGettingStarted']);
+        mockAngulartics2 = new Angulartics2Stub();
 
         TestBed.configureTestingModule({
             declarations: [
                 MapFooterComponent
             ],
             providers: [
+                { provide: AppSettingsService, useValue: mockAppSettings },
                 { provide: PinService, useValue: mockPinService },
                 { provide: LoginRedirectService, useValue: mockLoginRedirectService },
-                { provide: StateService, useValue: mockStateService },
-                { provide: SessionService, useValue: mockSessionService },
+                {
+                    provide: Router,
+                    useValue: { routerState: { snapshot: { url: '/map-footer-component' } } },
+                },
+                { provide: StateService, useValue: mockState },
+                { provide: SessionService, useValue: mockSession },
                 { provide: BlandPageService, useValue: mockBlandPageService },
                 { provide: UserLocationService, useValue: mockUserLocationService },
-                { provide: Angulartics2, useValue: mockAngulartics2 },
                 { provide: SearchService, useValue: mockSearchService },
-                { provide: AppSettingsService, useValue: mockAppSettingsService}
-            ],
-            imports: [
-                RouterTestingModule.withRoutes([])
+                { provide: Angulartics2, useValue: mockAngulartics2 }
             ],
             schemas: [NO_ERRORS_SCHEMA]
         });
@@ -81,17 +75,67 @@ describe('Component: MapFooter', () => {
         TestBed.compileComponents().then(() => {
             fixture = TestBed.createComponent(MapFooterComponent);
             comp = fixture.componentInstance;
+
+            // el = fixture.debugElement.query(By.css('h1'));
         });
     }));
 
     it('should create an instance', () => {
+        comp.ngOnInit();
         expect(comp).toBeTruthy();
     });
 
-    it('should change state to my stuff if it is inactive and "My Stuff" is clicked', () => {
-        spyOn(comp, 'changeStateToMyStuff');
+    it('myStuffBtnClicked when not in my stuff mode (Logged in)', () => {
+        mockState.myStuffActive = false;
+        (mockSession.isLoggedIn).and.returnValue(true);
         comp.myStuffBtnClicked();
-        expect(comp.changeStateToMyStuff).toHaveBeenCalled();
+        expect(mockAngulartics2.eventTrack.next).toHaveBeenCalledWith({ action: 'myStuff Button Click',
+               properties: { category: 'Connect' }});
+        expect(mockPinService.clearPinCache).toHaveBeenCalledTimes(1);
+        expect(mockState.setLoading).toHaveBeenCalledTimes(1);
+        expect(mockState.setCurrentView).toHaveBeenCalledWith('map');
+        expect(mockState.setMyViewOrWorldView).toHaveBeenCalledWith('my');
+        expect(mockState.setIsMyStuffActive).toHaveBeenCalledWith(true);
+        expect(mockPinService.emitPinSearchRequest).toHaveBeenCalledWith(new PinSearchRequestParams(true, null));
     });
 
+    it('myStuffBtnClicked should redirect to login when not logged in', () => {
+        mockState.myStuffActive = false;
+        (mockSession.isLoggedIn).and.returnValue(false);
+        comp.myStuffBtnClicked();
+        expect(mockAngulartics2.eventTrack.next).toHaveBeenCalledWith({ action: 'myStuff Button Click',
+               properties: { category: 'Connect' }});
+        expect(mockPinService.clearPinCache).toHaveBeenCalledTimes(1);
+        expect(mockLoginRedirectService.redirectToLogin).toHaveBeenCalledWith('/map-footer-component', comp.redirectThenChangeToMyStuff);
+    });
+
+    it('mystuffBtnClicked when my stuff mode is active', () => {
+        mockState.lastSearch.search = 'yay stuff';
+        mockState.myStuffActive = true;
+
+        comp.myStuffBtnClicked();
+
+        expect(mockAngulartics2.eventTrack.next).toHaveBeenCalledWith({ action: 'myStuff Button Click',
+               properties: { category: 'Connect' }});
+        expect(mockState.setIsMyStuffActive).toHaveBeenCalledWith(false);
+        expect(mockState.lastSearch.search).toBe('');
+        expect(mockState.setLoading).toHaveBeenCalledWith(true);
+        expect(mockState.setCurrentView).toHaveBeenCalledWith('map');
+        expect(mockState.setMyViewOrWorldView).toHaveBeenCalledWith('world');
+        expect(mockPinService.emitPinSearchRequest).toHaveBeenCalledWith(new PinSearchRequestParams(true, null));
+    });
+
+    it('redirectThenChangeToMyStuff should do change to mystuff and redirect to target', () => {
+        spyOn(comp, 'changeStateToMyStuff');
+        comp.redirectThenChangeToMyStuff();
+
+        expect(comp.changeStateToMyStuff).toHaveBeenCalledTimes(1);
+        expect(mockLoginRedirectService.redirectToTarget).toHaveBeenCalledTimes(1);
+    });
+
+    fit('should set state and go to getting started when gettingStartedBtnClicked', () =>{
+        comp.gettingStartedBtnClicked();
+        expect(mockState.setCurrentView).toHaveBeenCalledWith('map');
+        expect(mockBlandPageService.goToGettingStarted).toHaveBeenCalledTimes(1);
+    });
 });
--- src/app/components/map-footer/map-footer.component.ts
+++ src/app/components/map-footer/map-footer.component.ts
@@ -7,9 +7,7 @@ import { PinService } from '../../services/pin.service';
 import { LoginRedirectService } from '../../services/login-redirect.service';
 import { StateService } from '../../services/state.service';
 import { SessionService } from '../../services/session.service';
-import { UserLocationService } from  '../../services/user-location.service';
 import { BlandPageService } from '../../services/bland-page.service';
-import { SearchService } from '../../services/search.service';
 
 import { GeoCoordinates } from '../../models/geo-coordinates';
 import { Pin } from '../../models/pin';
@@ -26,14 +24,12 @@ export class MapFooterComponent implements OnInit {
   public myPinSearchResults: PinSearchResultsDto;
 
   constructor(private appSettings: AppSettingsService,
-              private pin: PinService,
+              private pinService: PinService,
               private loginRedirectService: LoginRedirectService,
               private router: Router,
               private state: StateService,
               private session: SessionService,
               private blandPageService: BlandPageService,
-              private userLocationService: UserLocationService,
-              private search: SearchService,
               private angulartics2: Angulartics2) { }
 
   public ngOnInit() {
@@ -47,7 +43,7 @@ export class MapFooterComponent implements OnInit {
 
   public myStuffBtnClicked(): void {
     this.angulartics2.eventTrack.next({ action: 'myStuff Button Click', properties: { category: 'Connect' }});
-
+    console.log(this.state.myStuffActive);
     if (this.state.myStuffActive) {
       this.changeStateToAllResults();
     } else {
@@ -60,24 +56,24 @@ export class MapFooterComponent implements OnInit {
     this.state.setIsMyStuffActive(false);
     this.state.lastSearch.search = '';
 
-    this.pin.clearPinCache();
+    this.pinService.clearPinCache();
 
     this.state.setLoading(true);
     this.state.setCurrentView('map');
     this.state.setMyViewOrWorldView('world');
 
     let pinSearchRequest = new PinSearchRequestParams(true, null);
-    this.pin.emitPinSearchRequest(pinSearchRequest);
+    this.pinService.emitPinSearchRequest(pinSearchRequest);
   }
 
 
-  public redirectThenChangeToMyStuff(){
+  public redirectThenChangeToMyStuff() {
     this.changeStateToMyStuff();
     this.loginRedirectService.redirectToTarget();
   }
 
   public changeStateToMyStuff(): void {
-    this.pin.clearPinCache();
+    this.pinService.clearPinCache();
 
     this.state.setLoading(true);
 
@@ -90,7 +86,7 @@ export class MapFooterComponent implements OnInit {
 
       let pinSearchRequest = new PinSearchRequestParams(true, null);
 
-      this.pin.emitPinSearchRequest(pinSearchRequest);
+      this.pinService.emitPinSearchRequest(pinSearchRequest);
     }
   }
 
