9107260e2e8c2da378bc510d964fc68061d5fd3c
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-08-01 10:46:41 -0400

    show modal on cancel

--- src/app/components/create-group/create-group-footer/create-group-footer.component.ts
+++ src/app/components/create-group/create-group-footer/create-group-footer.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { BlandPageService } from '../../../services/bland-page.service';
 import { CreateGroupService } from '../create-group-data.service';
 import { StateService } from '../../../services/state.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'create-group-footer',
   templateUrl: './create-group-footer.component.html',
 })
 export class CreateGroupFooterComponent implements OnInit {
+  @ViewChild('cancelModal') public cancelModal: ModalDirective;
   @Input() showbackbutton: boolean = true;
   @Output() backevent: EventEmitter<number> = new EventEmitter<number>();
   @Output() cancelevent: EventEmitter<number> = new EventEmitter<number>();
@@ -25,6 +27,24 @@ export class CreateGroupFooterComponent implements OnInit {
   }
 
   public OnCancel() {
-    this.cancelevent.emit(0);
+    this.showCancelModal();
   }
+
+  private showCancelModal(): void {
+      this.cancelModal.show();
+  }
+
+  private hideCancelModal(): void {
+      this.cancelModal.hide();
+  }
+
+  private cancelConfirmed(): void {
+      this.cancelModal.hide();
+      this.cancelevent.emit(0);
+  }
+
+  public cancelDeclined(): void {
+      this.cancelModal.hide();
+  }
+
 }
