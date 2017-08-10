1a483677eb1dd9acfe6f57ee577c73b056d6f2c7
harimau777 <moneysmith777@gmail.com>
2017-08-10 11:30:38 -0400

    Added test for switching to list view when Online filter is applied

--- src/app/components/filters/filters.component.spec.ts
+++ src/app/components/filters/filters.component.spec.ts
@@ -9,6 +9,7 @@ import { FilterService } from '../../services/filter.service';
 import { PinService } from '../../services/pin.service';
 import { StateService } from '../../services/state.service';
 import { FiltersComponent } from './filters.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component'
 
 import { AgeGroupsComponent } from './age-groups/age-groups.component';
 import { CategoryComponent } from './category/category.component';
@@ -21,106 +22,127 @@ import { MeetingFrequencyComponent } from './meeting-frequency/meeting-frequency
 
 import { SearchOptions } from '../../models';
 
+import { listViewType } from '../../shared/constants';
+
 describe('FiltersComponent', () => {
-    let fixture: ComponentFixture<FiltersComponent>;
-    let comp: FiltersComponent;
-    let mockFilterService, mockPinService, mockStateService, mockRouter;
+  let fixture: ComponentFixture<FiltersComponent>;
+  let comp: FiltersComponent;
+  let mockFilterService, mockPinService, mockStateService,
+    mockRouter, mockSearchBar, mockOnlineOrPhysicalGroupComponent;
 
-    beforeEach(() => {
-        mockFilterService = jasmine.createSpyObj<FilterService>('filterService', ['buildFilters', 'resetFilterString']);
-        mockPinService = jasmine.createSpyObj<PinService>('pinService', ['emitPinSearchRequest']);
-        mockStateService = jasmine.createSpyObj<StateService>('stateService',
-            ['emitPinSearchRequest', 'setMyViewOrWorldView', 'setIsFilterDialogOpen']);
-        mockRouter = jasmine.createSpyObj<Router>('router', ['navigate']);
+  beforeEach(() => {
+    mockFilterService = jasmine.createSpyObj<FilterService>('filterService', ['buildFilters', 'resetFilterString']);
+    mockPinService = jasmine.createSpyObj<PinService>('pinService', ['emitPinSearchRequest']);
+    mockStateService = jasmine.createSpyObj<StateService>('stateService',
+    ['setMyViewOrWorldView', 'setIsFilterDialogOpen', 'getCurrentView', 'setCurrentView']);
+    mockRouter = jasmine.createSpyObj<Router>('router', ['navigate']);
+    mockSearchBar = jasmine.createSpyObj<SearchBarComponent>('searchBar', ['setButtonText']);
 
-        TestBed.configureTestingModule({
-            declarations: [
-                FiltersComponent
-            ],
-            providers: [
-                { provide: FilterService, useValue: mockFilterService },
-                { provide: PinService, useValue: mockPinService },
-                { provide: StateService, useValue: mockStateService },
-                { provide: Router, useValue: mockRouter }
-            ],
-            schemas: [NO_ERRORS_SCHEMA],
-            imports: [ReactiveFormsModule, FormsModule]
-        });
+    TestBed.configureTestingModule({
+      declarations: [
+        FiltersComponent
+      ],
+      providers: [
+        { provide: FilterService, useValue: mockFilterService },
+        { provide: PinService, useValue: mockPinService },
+        { provide: StateService, useValue: mockStateService },
+        { provide: Router, useValue: mockRouter },
+        { provide: SearchBarComponent, useValue: mockSearchBar },
+        { provide: OnlineOrPhysicalGroupComponent, useValue: mockOnlineOrPhysicalGroupComponent}
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ReactiveFormsModule, FormsModule]
     });
+  });
 
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      this.fixture = TestBed.createComponent(FiltersComponent);
+      this.comp = this.fixture.componentInstance;
+      this.comp.locationFormGroup = new FormGroup({});
+    });
+  }));
+
+  it('should create an instance', () => {
+    expect(this.comp).toBeTruthy();
+  });
+
+  it('should init', () => {
+    this.comp.ngOnInit();
+    expect(this.comp.locationFormGroup).not.toBeUndefined();
+    expect(this.comp['locationFormGroup'].controls['location'].value).toBe(null);
+  });
+
+  it('should init with last saved search', () => {
+    this.comp['state'].lastSearch = new SearchOptions('', '', '45013');
+    this.comp.ngOnInit();
+    expect(this.comp['locationFormGroup'].controls['location'].value).toBe('45013');
+  });
+
+  it('should apply filters', () => {
+    this.comp.onlineOrPhysicalGroupComponent = jasmine.createSpyObj<OnlineOrPhysicalGroupComponent>('onlineOrPhysicalGroupComponent', ['getIsVirtualGroup']);
+
+    this.comp.searchKeywordString = 'my keywords';
+    this.comp['locationFormGroup'].controls['location'] = '90210';
+    this.comp['state'].lastSearch = new SearchOptions('words', 'filter me', '90210');
+
+    this.comp.onSubmit();
+    expect(mockStateService.setMyViewOrWorldView).toHaveBeenCalledWith('world');
+    expect(mockStateService.setIsFilterDialogOpen).toHaveBeenCalledWith(false);
+    expect(mockFilterService.buildFilters).toHaveBeenCalled();
+    expect(this.comp['state'].lastSearch.search).toBe('my keywords');
+  });
+
+  it('should switch to list view when the online groups filter is applied', () => {
+    this.comp.onlineOrPhysicalGroupComponent = jasmine.createSpyObj<OnlineOrPhysicalGroupComponent>('onlineOrPhysicalGroupComponent', ['getIsVirtualGroup']);
+
+    this.comp.searchKeywordString = 'my keywords';
+    this.comp['locationFormGroup'].controls['location'] = '90210';
+    this.comp['state'].lastSearch = new SearchOptions('words', 'filter me', '90210');
+
+    (this.comp.onlineOrPhysicalGroupComponent.getIsVirtualGroup).and.returnValue(true);
+    this.comp.onSubmit();
+    expect(this.comp['state'].setCurrentView).toHaveBeenCalledWith(listViewType);
+  });
+
+  describe('Reset', () => {
     beforeEach(async(() => {
-        TestBed.compileComponents().then(() => {
-            this.fixture = TestBed.createComponent(FiltersComponent);
-            this.comp = this.fixture.componentInstance;
-            this.comp.locationFormGroup = new FormGroup({});
-        });
+      this.comp['locationFormGroup'] = new FormGroup({
+        location: new FormControl(this.location, []),
+      });
+      this.comp['state'].lastSearch = new SearchOptions('', '', '');
+      this.comp.childKidsWelcomeComponent = jasmine.createSpyObj<KidsWelcomeComponent>('kidsWelcome', ['reset']);
+      this.comp.childAgeGroupsComponent = jasmine.createSpyObj<AgeGroupsComponent>('ageGroups', ['reset']);
+      this.comp.childCategoryComponent = jasmine.createSpyObj<CategoryComponent>('category', ['reset']);
+      this.comp.groupTypeComponent = jasmine.createSpyObj<GroupTypeComponent>('groupType', ['reset']);
+      this.comp.onlineOrPhysicalGroupComponent = jasmine.createSpyObj<OnlineOrPhysicalGroupComponent>('onlineOrPhysical', ['reset', 'getIsVirtualGroup']);
+
+      this.comp.meetingTimeComponent = jasmine.createSpyObj<MeetingTimeComponent>('onlineOrPhysical', ['reset']);
+      this.comp.meetingDayComponent = jasmine.createSpyObj<MeetingDayComponent>('onlineOrPhysical', ['reset']);
+      this.comp.meetingFrequencyComponent = jasmine.createSpyObj<MeetingFrequencyComponent>('onlineOrPhysical', ['reset']);
     }));
 
-    it('should create an instance', () => {
-        expect(this.comp).toBeTruthy();
+    it('should reset filters', () => {
+      this.comp['childKidsWelcomeComponent'].selected = true;
+      this.comp.resetFilters();
+      expect(this.comp.childKidsWelcomeComponent.reset).toHaveBeenCalled();
+      expect(this.comp.childAgeGroupsComponent.reset).toHaveBeenCalled();
+      expect(this.comp.childCategoryComponent.reset).toHaveBeenCalled();
+      expect(this.comp.groupTypeComponent.reset).toHaveBeenCalled();
+      expect(this.comp.onlineOrPhysicalGroupComponent.reset).toHaveBeenCalled();
+      expect(this.comp.meetingDayComponent.reset).toHaveBeenCalled();
+      expect(this.comp.meetingFrequencyComponent.reset).toHaveBeenCalled();
+      expect(this.comp.onlineOrPhysicalGroupComponent.reset).toHaveBeenCalled();
+
+      expect(this.comp.state.setIsFilterDialogOpen).toHaveBeenCalledWith(false);
+      expect(mockFilterService.resetFilterString).toHaveBeenCalled();
     });
 
-    it('should init', () => {
-        this.comp.ngOnInit();
-        expect(this.comp.locationFormGroup).not.toBeUndefined();
-        expect(this.comp['locationFormGroup'].controls['location'].value).toBe(null);
+    it('should reset location', () => {
+      this.comp['locationFormGroup'].controls['location'].setValue('90210');
+      this.comp.resetFilters();
+      expect(this.comp['locationFormGroup'].controls['location'].value).toBe('');
+      expect(this.comp['state'].lastSearch.location).toBe('');
     });
-
-    it('should init with last saved search', () => {
-        this.comp['state'].lastSearch = new SearchOptions('', '', '45013');
-        this.comp.ngOnInit();
-        expect(this.comp['locationFormGroup'].controls['location'].value).toBe('45013');
-    });
-
-    it('should apply filters', () => {
-        this.comp.searchKeywordString = 'my keywords';
-        this.comp['locationFormGroup'].controls['location'] = '90210';
-        this.comp['state'].lastSearch = new SearchOptions('words', 'filter me', '90210');
-        this.comp.onSubmit();
-        expect(mockStateService.setMyViewOrWorldView).toHaveBeenCalledWith('world');
-        expect(mockStateService.setIsFilterDialogOpen).toHaveBeenCalledWith(false);
-        expect(mockFilterService.buildFilters).toHaveBeenCalled();
-        expect(this.comp['state'].lastSearch.search).toBe('my keywords');
-    });
-
-    describe('Reset', () => {
-        beforeEach(async(() => {
-            this.comp['locationFormGroup'] = new FormGroup({
-                location: new FormControl(this.location, []),
-            });
-            this.comp['state'].lastSearch = new SearchOptions('', '', '');
-            this.comp.childKidsWelcomeComponent = jasmine.createSpyObj<KidsWelcomeComponent>('kidsWelcome', ['reset']);
-            this.comp.childAgeGroupsComponent = jasmine.createSpyObj<AgeGroupsComponent>('ageGroups', ['reset']);
-            this.comp.childCategoryComponent = jasmine.createSpyObj<CategoryComponent>('category', ['reset']);
-            this.comp.groupTypeComponent = jasmine.createSpyObj<GroupTypeComponent>('groupType', ['reset']);
-            this.comp.onlineOrPhysicalGroupComponent = jasmine.createSpyObj<OnlineOrPhysicalGroupComponent>('onlineOrPhysical', ['reset']);
-
-            this.comp.meetingTimeComponent = jasmine.createSpyObj<MeetingTimeComponent>('onlineOrPhysical', ['reset']);
-            this.comp.meetingDayComponent = jasmine.createSpyObj<MeetingDayComponent>('onlineOrPhysical', ['reset']);
-            this.comp.meetingFrequencyComponent = jasmine.createSpyObj<MeetingFrequencyComponent>('onlineOrPhysical', ['reset']);
-        }));
-
-        it('should reset filters', () => {
-            this.comp['childKidsWelcomeComponent'].selected = true;
-            this.comp.resetFilters();
-            expect(this.comp.childKidsWelcomeComponent.reset).toHaveBeenCalled();
-            expect(this.comp.childAgeGroupsComponent.reset).toHaveBeenCalled();
-            expect(this.comp.childCategoryComponent.reset).toHaveBeenCalled();
-            expect(this.comp.groupTypeComponent.reset).toHaveBeenCalled();
-            expect(this.comp.onlineOrPhysicalGroupComponent.reset).toHaveBeenCalled();
-            expect(this.comp.meetingDayComponent.reset).toHaveBeenCalled();
-            expect(this.comp.meetingFrequencyComponent.reset).toHaveBeenCalled();
-            expect(this.comp.onlineOrPhysicalGroupComponent.reset).toHaveBeenCalled();
-
-            expect(this.comp.state.setIsFilterDialogOpen).toHaveBeenCalledWith(false);
-            expect(mockFilterService.resetFilterString).toHaveBeenCalled();
-        });
-
-        it('should reset location', () => {
-            this.comp['locationFormGroup'].controls['location'].setValue('90210');
-            this.comp.resetFilters();
-            expect(this.comp['locationFormGroup'].controls['location'].value).toBe('');
-            expect(this.comp['state'].lastSearch.location).toBe('');
-        });
-    });
+  });
 });
