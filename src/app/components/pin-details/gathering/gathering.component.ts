918ce20cb087e4e65f12a52957cad0eacadeb741
harimau777 <moneysmith777@gmail.com>
2017-08-14 13:45:15 -0400

    Navigating to group details directly via URL now works

--- src/app/components/pin-details/gathering/gathering.component.ts
+++ src/app/components/pin-details/gathering/gathering.component.ts
@@ -31,7 +31,6 @@ import * as moment from 'moment';
   templateUrl: 'gathering.html'
 })
 export class GatheringComponent implements OnInit {
-
   @Input() pin: Pin;
   @Input() user: Pin;
   @Input() isPinOwner: boolean = false;
@@ -67,7 +66,6 @@ export class GatheringComponent implements OnInit {
     public appSettingsService: AppSettingsService) { }
 
   // ONINIT is doing WAY too much, needs to be simplified and broken up.
-
   public ngOnInit() {
     if (!this.previewMode) {
       window.scrollTo(0, 0);
@@ -137,9 +135,11 @@ export class GatheringComponent implements OnInit {
 
   public getProximityString(): string {
     if (this.isOnlineGroup()) {
-      return 'ONLINE GROUP';
+      return '(ONLINE GROUP)';
+    } else if (this.pin.proximity) {
+      return `(${this.pin.proximity.toFixed(1)} MI)`;
     } else {
-      return `${this.pin.proximity.toFixed(1)} MI`;
+      return '';
     }
   }
 
