e66408c021635eeb92be8be17e61b9e5aedf7b27
harimau777 <moneysmith777@gmail.com>
2017-08-15 08:42:09 -0400

    Trial member approval or disapproval now sent to the backend endpoint

--- src/app/components/pin-details/pin-details.component.ts
+++ src/app/components/pin-details/pin-details.component.ts
@@ -17,7 +17,6 @@ import { User } from '../../models/user';
   templateUrl: 'pin-details.html'
 })
 export class PinDetailsComponent implements OnInit {
-
   @Input() pin: Pin;
   public submitted: boolean = false;
   public errorMessage: string = '';
@@ -29,10 +28,7 @@ export class PinDetailsComponent implements OnInit {
   public sayHiText: string = '';
   public isInGathering: boolean = false;
   public user: Pin;
-
-  // Attributes used for trial member approval:
-  private approved: boolean;
-  private trialMemberId: number;
+  private trialMemberApprovalMessage: string;
 
   constructor(
     private location: PlatformLocation,
@@ -41,7 +37,7 @@ export class PinDetailsComponent implements OnInit {
     private session: SessionService,
     private state: StateService,
     private pinService: PinService
-  ) { }
+  ) {}
 
   public ngOnInit() {
     this.state.setLoading(true);
@@ -50,12 +46,7 @@ export class PinDetailsComponent implements OnInit {
     this.pin = this.route.snapshot.data['pin'];
     this.user = this.route.snapshot.data['user'];
 
-    if(this.route.snapshot.paramMap.get('approved')) {
-      this.approved = this.route.snapshot.paramMap.get('approved').toLowerCase() === 'true';
-    }
-    if(this.route.snapshot.paramMap.get('trialMemberId')) {
-      this.trialMemberId = Number(this.route.snapshot.paramMap.get('trialMemberId'));
-    }
+    this.approveOrDisapproveTrialMember();
 
     if (this.pin.pinType === pinType.GATHERING) {
       this.isGatheringPin = true;
@@ -67,10 +58,25 @@ export class PinDetailsComponent implements OnInit {
       this.isLoggedIn = true;
       this.isPinOwner = this.doesLoggedInUserOwnPin();
     }
+
     this.state.setLoading(false);
   }
 
   private doesLoggedInUserOwnPin() {
     return this.pinService.doesLoggedInUserOwnPin(this.pin);
+  }
+
+  private approveOrDisapproveTrialMember() {
+    const approved: boolean = (this.route.snapshot.paramMap.get('approved') === 'true');
+    const trialMemberId: string = this.route.snapshot.paramMap.get('trialMemberId');
+    const baseUrl = process.env.CRDS_GATEWAY_CLIENT_ENDPOINT;
+
+    if(approved && trialMemberId) {
+      this.session.post(`${baseUrl}api/v1.0.0/finder/pin/tryagroup/${this.pin.gathering.groupId}/${approved}/${trialMemberId}`, null)
+      .subscribe(
+        success => this.trialMemberApprovalMessage = approved ? 'Trial member was approved' : 'Trial member was disapproved',
+        failure => this.trialMemberApprovalMessage = 'Error approving trial member'
+      );
+    }
   }
 }
