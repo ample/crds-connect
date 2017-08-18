f8e5dfdd2ee12f6468cf2b0e5ee06d7b4d34b53e
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-08-18 11:42:30 -0400

    DE4123 pull out func

--- src/app/components/create-group/preview/create-group-preview.component.ts
+++ src/app/components/create-group/preview/create-group-preview.component.ts
@@ -10,6 +10,7 @@ import {ProfileService} from '../../../services/profile.service';
 import {GroupService} from '../../../services/group.service';
 import {StateService} from '../../../services/state.service';
 import {CreateGroupService} from '../create-group-data.service';
+import {TimeHelperService} from '../../../services/time-helper.service';
 import {Pin, Participant} from '../../../models';
 import {Component, OnInit} from '@angular/core';
 import {ViewType, groupPaths, GroupPageNumber, textConstants} from '../../../shared/constants';
@@ -34,7 +35,8 @@ export class CreateGroupPreviewComponent implements OnInit {
               private participantService: ParticipantService,
               private pinService: PinService,
               private blandPageService: BlandPageService,
-              private contentService: ContentService) {
+              private contentService: ContentService,
+              private timeHelperService: TimeHelperService) {
   }
 
   ngOnInit() {
@@ -64,13 +66,7 @@ export class CreateGroupPreviewComponent implements OnInit {
       )
         .subscribe((returnData) => {
           this.toastr.success('Successfully edited group!');
-
-//
-          let dt = new Date(this.smallGroupPin.gathering.meetingTime);
-          let easterTime = new Date(dt.getTime() - (dt.getTimezoneOffset() * 60000)).toISOString();
-          this.smallGroupPin.gathering.meetingTime = easterTime;
-//
-
+          this.smallGroupPin.gathering.meetingTime = this.timeHelperService.convertTime(this.smallGroupPin.gathering.meetingTime);
           this.state.postedPin = this.smallGroupPin;
           this.state.setIsMyStuffActive(true);
           this.state.setCurrentView(ViewType.LIST);
