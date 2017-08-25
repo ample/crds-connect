7b9501b40518a35d69e214a301e31484c28af66e
vchtel <VictorChtelmakh@gmail.com>
2017-08-25 11:33:35 -0400

    Path was relative to parent path. When we came from email link, path was incorrect, so navigation broke

--- src/app/components/pin-details/gathering/participant-card/participant-card.component.ts
+++ src/app/components/pin-details/gathering/participant-card/participant-card.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { SessionService } from '../../../../services/session.service';
 import { AppSettingsService } from '../../../../services/app-settings.service';
+
+import { Group } from '../../../../models/group';
 import { Participant } from '../../../../models/participant';
+
 import { GroupRole } from '../../../../shared/constants';
 
 @Component({
@@ -14,6 +17,7 @@ export class ParticipantCardComponent implements OnInit {
 
   @Input() participant: Participant;
   @Input() pinParticipantId: number;
+  @Input() groupCardIsDisplayedOn: Group;
   public isLeader: boolean = false;
   public isMe: boolean = false;
   public isApprentice: boolean = false;
@@ -51,7 +55,9 @@ export class ParticipantCardComponent implements OnInit {
 
   public onParticipantClick(): void {
       if (this.participant.canBeHyperlinked) {
-        this.router.navigate(['./participant-detail/' + this.participant.groupParticipantId], { relativeTo: this.route });
+        let routeToNavigateTo: string = `/small-group/${this.groupCardIsDisplayedOn.groupId}/participant-detail/${this.participant.groupParticipantId}`;
+        this.router.navigate([routeToNavigateTo],
+          { relativeTo: this.route });
     }
   }
 
