56f6501f0e4f71425f7d443e969ccabecba779df
harimau777 <moneysmith777@gmail.com>
2017-09-11 10:13:11 -0400

    Removing dead code from test for Map and Search Local components

--- src/app/components/search-local/search-local.component.spec.ts
+++ src/app/components/search-local/search-local.component.spec.ts
@@ -1,25 +1,18 @@
-import { AnalyticsService } from '../../services/analytics.service';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CookieOptionsArgs, CookieService } from 'angular2-cookie/core';
 import { EventEmitter } from '@angular/core';
 import { GoogleMapsAPIWrapper } from 'angular2-google-maps/core';
 import { Http, RequestOptions, Response } from '@angular/http';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { AnalyticsService } from '../../services/analytics.service';
 import { AppSettingsService } from '../../services/app-settings.service';
-import { BlandPageService } from '../../services/bland-page.service';
 import { GoogleMapService } from '../../services/google-map.service';
-import { IPService } from '../../services/ip.service';
-import { LocationService } from '../../services/location.service';
-import { LoginRedirectService } from '../../services/login-redirect.service';
-import { NeighborsHelperService } from '../../services/neighbors-helper.service';
 import { PinLabelService } from '../../services/pin-label.service';
 import { PinService } from '../../services/pin.service';
 import { SearchService } from '../../services/search.service';
 import { SessionService } from '../../services/session.service';
-import { SiteAddressService } from '../../services/site-address.service';
 import { StateService } from '../../services/state.service';
 import { UserLocationService } from '../../services/user-location.service';
 
@@ -30,51 +23,25 @@ import { MapContentComponent } from '../../components/map-content/map-content.co
 import { MapFooterComponent } from '../map-footer/map-footer.component';
 import { SearchLocalComponent } from '../search-local/search-local.component';
 import { MapSettings } from '../../models/map-settings';
-import { MapView } from '../../models/map-view';
 
 describe('Component: SearchLocal', () => {
   let fixture: ComponentFixture<SearchLocalComponent>;
   let comp: SearchLocalComponent;
-  let mockStateService,
-    mockSessionService,
-    mockAnaltyics,
-    mockUserLocationService,
-    mockLocationService,
-    mockSiteAddressService,
-    mockPinService,
-    mockPinLabelService,
-    mockGoogleMapService,
-    mockGoogleMapsAPIWrapper,
-    mockNeighborsHelperService,
-    mockSearchService,
-    mockLoginRedirectService,
-    mockBlandPageService,
-    mockIPService,
-    mockCookieService,
-    mockEventEmitter,
-    mockAppSettingsService;
-
+  let mockStateService, mockSessionService, mockAnaltyics, mockUserLocationService,
+    mockPinService, mockPinLabelService, mockGoogleMapService, mockSearchService,
+    mockAppSettingsService, mockEventEmitter;
 
   beforeEach(() => {
     mockStateService = jasmine.createSpyObj<StateService>('state', ['']);
-    mockSessionService = jasmine.createSpyObj<SessionService>('session', ['getContactId', 'isLoggedIn']);
-    mockAnaltyics = jasmine.createSpyObj<AnalyticsService>('analytics', ['updateResultsPressed']);
-    mockSiteAddressService = jasmine.createSpyObj<SiteAddressService>('siteAddressService', ['']);
-    mockUserLocationService = jasmine.createSpyObj<UserLocationService>('userLocationService', ['GetUserLocation']);
-    mockLocationService = jasmine.createSpyObj<LocationService>('locationService', ['getCurrentPosition']);
-    mockPinService = jasmine.createSpyObj<PinService>('pinService', ['getPinSearchResults']);
-    mockPinLabelService = jasmine.createSpyObj<PinLabelService>('pinLabelService', ['constructor']);
-    mockGoogleMapService = jasmine.createSpyObj<GoogleMapService>('googleMapService', ['constructor', 'setDidUserAllowGeoLoc']);
-    mockGoogleMapsAPIWrapper = jasmine.createSpyObj<GoogleMapsAPIWrapper>('googleMapAPIWrapper', ['constructor']);
-    mockNeighborsHelperService = jasmine.createSpyObj<NeighborsHelperService>('neighborsHelperService', ['']);
+    mockSessionService = jasmine.createSpyObj<SessionService>('session', ['']);
+    mockAnaltyics = jasmine.createSpyObj<AnalyticsService>('analytics', ['']);
+    mockUserLocationService = jasmine.createSpyObj<UserLocationService>('userLocationService', ['']);
+    mockPinService = jasmine.createSpyObj<PinService>('pinService', ['']);
+    mockPinLabelService = jasmine.createSpyObj<PinLabelService>('pinLabelService', ['']);
+    mockGoogleMapService = jasmine.createSpyObj<GoogleMapService>('googleMapService', ['']);
     mockSearchService = jasmine.createSpyObj<SearchService>('searchService', ['']);
-    mockSessionService = jasmine.createSpyObj<SessionService>('sessionService', ['getContactId', 'get', 'isLoggedIn']);
-    mockLoginRedirectService = jasmine.createSpyObj<LoginRedirectService>('loginRedirectService', ['']);
-    mockBlandPageService = jasmine.createSpyObj<BlandPageService>('blandPageService', ['']);
-    mockIPService = jasmine.createSpyObj<IPService>('ipService', ['']);
-    mockCookieService = jasmine.createSpyObj<CookieService>('cookieService', ['constructor']);
+    mockAppSettingsService = jasmine.createSpyObj<AppSettingsService>('appSettingService', ['']);
     mockEventEmitter = jasmine.createSpyObj<EventEmitter<MapView>>('eventEmitter', ['subscribe']);
-    mockAppSettingsService = jasmine.createSpyObj<AppSettingsService>('appSettingService', ['constructor']);
 
     TestBed.configureTestingModule({
       declarations: [
@@ -93,21 +60,21 @@ describe('Component: SearchLocal', () => {
       ],
       providers: [
         { provide: UserLocationService, useValue: mockUserLocationService },
-        { provide: LocationService, useValue: mockLocationService },
+        // { provide: LocationService, useValue: mockLocationService },
         { provide: PinLabelService, useValue: mockPinLabelService },
         { provide: PinService, useValue: mockPinService },
-        { provide: SiteAddressService, useValue: mockSiteAddressService },
+        // { provide: SiteAddressService, useValue: mockSiteAddressService },
         { provide: GoogleMapService, useValue: mockGoogleMapService },
         { provide: StateService, useValue: mockStateService },
         { provide: SessionService, useValue: mockSessionService },
         { provide: AnalyticsService, useValue: mockAnaltyics },
-        { provide: LoginRedirectService, useValue: mockLoginRedirectService },
-        { provide: BlandPageService, useValue: mockBlandPageService },
-        { provide: GoogleMapsAPIWrapper, useValue: mockGoogleMapsAPIWrapper },
-        { provide: CookieService, useValue: mockCookieService },
+        // { provide: LoginRedirectService, useValue: mockLoginRedirectService },
+        // { provide: BlandPageService, useValue: mockBlandPageService },
+        // { provide: GoogleMapsAPIWrapper, useValue: mockGoogleMapsAPIWrapper },
+        // { provide: CookieService, useValue: mockCookieService },
         { provide: SearchService, useValue: mockSearchService },
-        { provide: IPService, useValue: mockIPService },
-        { provide: NeighborsHelperService, useValue: mockNeighborsHelperService },
+        // { provide: IPService, useValue: mockIPService },
+        // { provide: NeighborsHelperService, useValue: mockNeighborsHelperService },
         { provide: AppSettingsService, useValue: mockAppSettingsService}
       ]
     });
@@ -141,5 +108,4 @@ describe('Component: SearchLocal', () => {
     comp.ngOnInit();
     expect(comp.active).toBe(false);
   });
-
 });
