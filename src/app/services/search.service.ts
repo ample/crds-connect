cb31b1637a3fcd496e634339b563abb2d2d95107
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-09-14 12:59:07 -0400

    Call the function not check if the function exists

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -4,8 +4,7 @@ import { AppSettingsService } from '../services/app-settings.service';
 import { StateService } from '../services/state.service';
 
 import { Pin } from '../models/pin';
-
-import { ViewType } from '../shared/constants'
+import { ViewType } from '../shared/constants';
 
 @Injectable()
 export class SearchService {
@@ -13,10 +12,10 @@ export class SearchService {
   constructor(
     private appSettings: AppSettingsService,
     private state: StateService,
-  ) {}
+  ) { }
 
   public navigateToListViewIfInGroupToolAndAllGroupsOnline(pinsReturnedBySearch: Pin[]) {
-    if(this.appSettings.isSmallGroupApp && this.areAllReturnedGroupsOnlineGroups(pinsReturnedBySearch)){
+    if (this.appSettings.isSmallGroupApp() && this.areAllReturnedGroupsOnlineGroups(pinsReturnedBySearch)) {
       this.state.setCurrentView(ViewType.LIST);
     }
   }
