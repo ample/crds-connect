479966c3937cac39c02203f5dbdd6a7e61a50799
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-09-20 13:53:35 -0400

    Add all environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  crdsEnv: '',
+  CRDS_GATEWAY_CLIENT_ENDPOINT: 'https://gateway.crossroads.net/gateway/',
+  CRDS_ENV: '',
+  CRDS_COOKIE_DOMAIN: 'crossroads.net',
+  cookieDomain: 'crossroads.net',
+  apiEndpoint: 'https://gateway.crossroads.net/gateway/',
+  crdsEndpoint: 'https://content.crossroads.net/',
+  googleApiKey: 'AIzaSyCKpuQaaHMygON751DgjreluhUF3_CMVSE',
+  crossroadsApiKey: '',
+  CRDS_SERVICES_CLIENT_ENDPOINT: 'https://gateway.crossroads.net/',
+  CRDS_CMS_CLIENT_ENDPOINT: 'https://content.crossroads.net/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@
 export const environment = {
   production: false,
   crdsEnv: 'local',
-  CRDS_GATEWAY_CLIENT_ENDPOINT: 'https://gatewayint.crossroads.net/gateway/',
-  CRDS_ENV: 'int',
+  CRDS_GATEWAY_CLIENT_ENDPOINT: 'http://localhost:49380/',
+  CRDS_ENV: 'local',
   CRDS_COOKIE_DOMAIN: '.crossroads.net',
   cookieDomain: '',
-  apiEndpoint: 'https://gatewayint.crossroads.net/gateway/',
+  apiEndpoint: 'http://localhost:49380/',
   crdsEndpoint: 'https://contentint.crossroads.net/',
   googleApiKey: 'AIzaSyArKsBK97N0Wi-69x10OL7Sx57Fwlmu6Cs',
   crossroadsApiKey: '',
-  CRDS_SERVICES_CLIENT_ENDPOINT: 'https://gatewayint.crossroads.net/',
+  CRDS_SERVICES_CLIENT_ENDPOINT: 'http://localhost:49380/',
   CRDS_CMS_CLIENT_ENDPOINT: 'https://contentint.crossroads.net/'
 };
