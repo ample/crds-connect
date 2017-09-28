2eda009f1230481a382e2392c802831e060c6bc6
harimau777 <moneysmith777@gmail.com>
2017-09-28 13:24:15 -0400

    Refactored the google-map-cluster directive

--- src/app/directives/google-map-cluster.directive.ts
+++ src/app/directives/google-map-cluster.directive.ts
@@ -8,19 +8,7 @@ import '../../scripts/markerclusterer.js';
 
 declare const MarkerClusterer;
 
-interface IMarkerManager {
-  _markers?: any;
-  next(): any;
-}
-
-interface IObjectWithIcon {
-  icon?: any;
-}
-
-// MarkerManager.prototype.markerKeys = function() { return this._markers.keys(); }
-
 // An Attribute Directive that creates a MarkerClusterer in the map once the markers are loaded.
-
 @Directive({ selector: 'google-map-cluster' })
 export class GoogleMapClusterDirective implements AfterContentInit {
   private map: GoogleMap;
@@ -30,11 +18,11 @@ export class GoogleMapClusterDirective implements AfterContentInit {
     private mapHlpr: GoogleMapService,
     private wrapper: GoogleMapsAPIWrapper,
     private markerManager: MarkerManager,
-  ) { }
+  ) {}
 
   ngAfterContentInit() {
     this.wrapper.getNativeMap().then(map => {
-      let options = {
+      const options = {
         // maxZoom: 16, grey starts here for groups
         // maxZoom:17, grey does not seem to appear, BUT
         // all labels don't display and markers on top of each other, hard/impossible to click on individual pins
@@ -47,25 +35,8 @@ export class GoogleMapClusterDirective implements AfterContentInit {
           textColor: '#fff'
         }]
       };
-      let sebmMarkers = <IMarkerManager>this.markerManager['_markers'].keys(); // markerKeys();
-      let markers = [];
-      let siteMarkers = [];
-      let promises = [];
-      let sebmMarker;
 
-      while (!(sebmMarker = sebmMarkers.next()).done) {
-        sebmMarker = sebmMarker.value;
-        let promise = this.markerManager.getNativeMarker(sebmMarker);
-        promises.push(promise);
-        promise.then(marker => {
-          markers.push(marker);
-        });
-      }
-
-      Promise.all(promises).then(() => {
-        this.cluster = new MarkerClusterer(map, markers, options);
-      });
-
+      this.cluster = new MarkerClusterer(map, this.markerManager['_markers'], options);
     });
   }
 }
