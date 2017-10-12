018168e8f17ef1b5153da9977639f04ef8bd37a2
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-10-12 07:43:04 -0400

    use proxy on CRDS_GATEWAY_CLIENT_ENDPOINT

--- src/environments/environment.int.ts
+++ src/environments/environment.int.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   crdsEnv: 'int',
-  CRDS_GATEWAY_CLIENT_ENDPOINT: 'https://gatewayint.crossroads.net/gateway/',
+  CRDS_GATEWAY_CLIENT_ENDPOINT: '/proxy/gateway/',
   CRDS_ENV: 'int',
   CRDS_COOKIE_DOMAIN: '.crossroads.net',
   cookieDomain: '',
